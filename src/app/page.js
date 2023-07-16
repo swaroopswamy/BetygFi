72525a73779c454e9018549393a369b2b0e245c8
unknown <rajeshchaudhary16@live.com>
2023-07-16 06:09:37 -0700

    Fixes: imports removed

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,6 @@
 "use client"
-import styles from "./page.module.css";
 import { redirect } from 'next/navigation';
-import { Box, Grid, GridItem, SimpleGrid, } from "@chakra-ui/react";
-import Navbar from "./components/header";
 import styles from "./page.module.css";
-import Rankings from "./pages/Home/Rankings";
-import Overview from "./pages/Home/Overview";
-import ScoreDistribution from "./pages/Home/ScoreDistribution";
-import About from "./pages/about";
-import TermsandConditions from "./pages/terms";
 
 export default function Home() {
   redirect('/dashboard');
