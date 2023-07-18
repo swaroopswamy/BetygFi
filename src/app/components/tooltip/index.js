a7c335d90b787f6486bec84b10a2919582699e27
George Michael <george@Georges-MacBook-Pro.local>
2023-07-18 21:06:12 +0530

    Build Fix

--- src/app/components/tooltip/index.js
+++ src/app/components/tooltip/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tooltip, useColorModeValue } from '@chakra-ui/react'
+import { Tooltip, useColorModeValue, Text } from '@chakra-ui/react'
 
 const CustomTooltip = ({ children }) => {
     return (
