fdf5174aeb2bad192e9de85758f8c4399e0eb029
Varun Manjunatha <varun.m@solvendo.io>
2023-07-24 18:46:52 +0530

    style changes

--- src/app/help/page.js
+++ src/app/help/page.js
@@ -1,39 +1,55 @@
 "use client"
-import { Image } from '@chakra-ui/react'
+// import { Image } from '@chakra-ui/react'
 import { Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon,} from '@chakra-ui/react'
 import { Box, Container, Grid, GridItem, Heading, Text, extendTheme, Manrope } from "@chakra-ui/react";
-import styles from "../Help/help.module.css";
-import { useControllableState } from "@chakra-ui/react-use-controllable-state";
-import { mergeRefs } from "@chakra-ui/react-use-merge-refs";
-import { callAllHandlers, warn } from "@chakra-ui/shared-utils";
-import { useCallback, useEffect, useId, useRef, useState } from "react";
-
 
 const Help = () => {
     return (
         <>
-            <Container className={styles.container}>
-                <Box className={styles.box1}>
+            <Container  padding= {"0px"} maxW="100%">
+                <Box height= {"264px"}  background= "#E8E8E8">
                     <Grid>
-                        <GridItem colSpan={2} colStart={0} colEnd={2} className={styles.helpgrid}>
+                        <GridItem colSpan={2} colStart={0} colEnd={2} 
+                                  height= {"250px"}  
+                                  flex-shrink= {"0"} 
+                                  background= {"#E8E8E8"} 
+                                  mix-blend-mode= {"luminosity"}
+                                  padding= {"109px 0px 109px 70px"} 
+                                  color= {"#000"}  
+                                  fontFamily= {"Manrope"}
+                                  fontSize= {"46px"}
+                                  fontStyle= {"normal"} 
+                                  fontWeight= {400} 
+                                  lineHeight= {"46px"}>
                             Help
                         </GridItem>
                         {/* <GridItem colSpan={2} colStart={2} colEnd={4}>
-                            <i className={styles.logo} style={{padding: '40px 10px 20px 50px'}}>
+                            <i fill= linear-gradient(177deg, #F6F6F6 0%, #E7E7EC 100%)
+                              width= {"225.315px"}
+                              height= {"117.839px"}
+                              transform= {"rotate(113.629deg)"}"
+                              flex-shrink= {"0"} 
+                              padding= {"40px 10px 20px 50px}>
                             <Image src='../../../assests/images/solvendo-logo.svg' fallbackSrc='http://www.w3.org/1999/xlink' />
                             </i>
                         </GridItem> */}
                     </Grid>
                 </Box>
 
-                <Text className={styles.faq} padding={"10px 0px 10px 70px"}>
+                <Text color= {"#191919"}  
+                      fontFamily= {"Manrope"}  
+                      fontSize= {"22px"}  
+                      fontStyle= {"normal"}
+                      fontWeight= {400}  
+                      lineHeight= {"46px"}  
+                      padding={"10px 0px 10px 70px"}>
                      FAQs   
                 </Text>
 
-  <Accordion defaultIndex={[0]} allowMultiple  style={{padding:'10px 80px 80px 70px'}}>
+  <Accordion defaultIndex={[0]} allowMultiple  padding={'10px 80px 80px 70px'}>
   <AccordionItem>
       <AccordionButton>
-        <Box as="span" flex='1' textAlign='left'  >
+        <Box as="span" flex='1' textAlign='left' >
           What is ethereum?
         </Box>
         <AccordionIcon />
--- src/app/privacy/page.js
+++ src/app/privacy/page.js
@@ -1,14 +1,25 @@
 "use client"
-import { Box, Container, Grid, GridItem, Heading, Text, extendTheme, Manrope } from "@chakra-ui/react";
+import { Box, Container, Grid, GridItem,  Text } from "@chakra-ui/react";
 import styles from "../page.module.css";
 
  const Privacy = () => {
     return (
         <>
-            <Container className={styles.container}>
-                <Box className={styles.box1}>
+            <Container  maxW= {"100%"}  padding= {"0px"}>
+                <Box width= {"100%"}  height= {"264px"}  background= {"#E8E8E8"}>
                     <Grid>
-                        <GridItem colSpan={2} colStart={0} colEnd={2} className={styles.privacygrid}>
+                        <GridItem colSpan={2} colStart={0} colEnd={2}
+                        height= {"250px"}
+                        flexShrink= {"0"}
+                        background= {"#E8E8E8"}
+                        mix-blend-mode= {"luminosity"}
+                        padding= {"109px 0px 0px 70px"}  
+                        color= {"#000"}  
+                        fontFamily= {"Manrope"}  
+                        fontSize= {"46px"}  
+                        fontStyle= {"normal"}  
+                        fontWeight= {"400"}  
+                        lineHeight= {"46px"}>
                             Privacy Policy
                         </GridItem>
                         <GridItem colSpan={2} colStart={2} colEnd={4}>
@@ -17,28 +28,63 @@ import styles from "../page.module.css";
                     </Grid>
                 </Box>
 
-                <Box className={styles.privacybox2}>
-                    <Text className={styles.privacytext} >
+                <Box  width= {"75%"}  height= {"603px"}>
+                    <Text  opacity= {"0.800000011920929"}
+                           color= {"#191919"}
+                           fontFamily= {"Manrope"}
+                           fontSize= {"14px"}   
+                           fontStyle= {"normal"}
+                           fontWeight= {"400"}
+                           lineHeight= {"26px"}
+                           padding= {"10px 0px 0px 70px"} >
                         This is a common question, and the first thing to make clear is that we do not focus on personal individuals in our wallet labels.
                     </Text>
 
-                    <Text className={styles.privacytext}>
+                    <Text  opacity= {"0.800000011920929"}
+                           color= {"#191919"}
+                           fontFamily= {"Manrope"}
+                           fontSize= {"14px"}   
+                           fontStyle= {"normal"}
+                           fontWeight= {"400"}
+                           lineHeight= {"26px"}
+                           padding= {"10px 0px 0px 70px"} >
                         One important source of data for BetygFi is the blockchain itself. It's important to understand that blockchains are public and immutable. We have no control of what users decide to publish on them.
                         A relevant example is the Ethereum Name Service (ENS), which lets users purchase a human-readable name for their wallet. When a wallet registers such a domain (or is involved in the transfer of one), this is recorded forever on the Ethereum blockchain. And since anyone with a computer and an internet connection can run an Ethereum node, the data point representing this transaction is openly available to the world.
                     </Text>
 
-                    <Text className={styles.privacytext}>
+                    <Text  opacity= {"0.800000011920929"}
+                           color= {"#191919"}
+                           fontFamily= {"Manrope"}
+                           fontSize= {"14px"}   
+                           fontStyle= {"normal"}
+                           fontWeight= {"400"}
+                           lineHeight= {"26px"}
+                           padding= {"10px 0px 0px 70px"} >
                         Nansen includes information about ENS ownership of wallets (both past and present), and in some cases this might be revealing of an association between a private individual and a wallet.
                         So while we could remove data points potentially revealing personal identities from our platform,
                         we think it's better that they're made aware that this information is already in the public domain - accessible to anyone.‍
                     </Text>
 
-                    <Text className={styles.privacytext}>
+                    <Text  opacity= {"0.800000011920929"}
+                           color= {"#191919"}
+                           fontFamily= {"Manrope"}
+                           fontSize= {"14px"}   
+                           fontStyle= {"normal"}
+                           fontWeight= {"400"}
+                           lineHeight= {"26px"}
+                           padding= {"10px 0px 0px 70px"} >
                         We are committed to educating users of the privacy risks involved in using blockchains.‍
                         In some cases, project co-founders, advisors, and otherwise public figures have shared their addresses in the public domain (e.g. in transparency reports, or news articles). In these cases we might add wallet labels with their names, as the information is already in the public domain.
                     </Text>
 
-                    <Text className={styles.privacytext}>
+                    <Text  opacity= {"0.800000011920929"}
+                           color= {"#191919"}
+                           fontFamily= {"Manrope"}
+                           fontSize= {"14px"}   
+                           fontStyle= {"normal"}
+                           fontWeight= {"400"}
+                           lineHeight= {"26px"}
+                           padding= {"10px 0px 0px 70px"} >
                         You can also review our privacy policy information about the privacy of our customers' data.
                     </Text>
                 </Box>
