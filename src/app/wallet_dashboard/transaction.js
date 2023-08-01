a85e67bc0ecfe1c1b96ec7bd70f82a749d38bd80
Varun Manjunatha <varun.m@solvendo.io>
2023-08-01 12:50:34 +0530

    transaction updating

--- src/app/wallet_dashboard/transaction.js
+++ src/app/wallet_dashboard/transaction.js
@@ -2,18 +2,25 @@
 import { Container, Table, Thead,Text, Tbody,useColorModeValue,Box, Tfoot, Tr, Th, Td, TableCaption, TableContainer, Flex } from '@chakra-ui/react'
 
 const TransactionPanelComponent = () => {
+
+  const value1 = "300" ;
+  const value2="200";
+  const value3 = "-300";
   return (
     <>
-      <Flex justifyContent={"space-between"} padding={"23px 29px 27px"}>
+      <Flex justifyContent={"space-between"} padding={"23px 29px 27px"}
+             bgColor={useColorModeValue("#FFF","#202020")} >
         <Text
           fontSize="2xl"
           fontWeight={"400"}
           color={useColorModeValue("#16171B", "#FFF")}
+          
+          
         >
           Wallet Transaction
         </Text>
       </Flex>
-      <Table variant="simple" key={1}>
+      <Table variant="simple" key={1} bgColor={"#FFF"}>
         <Thead bgColor={useColorModeValue("#F5F5F7", "#191919")}>
           <Tr>
             <Th
@@ -25,22 +32,51 @@ const TransactionPanelComponent = () => {
             >
               ADDRESS AND DATE
             </Th>
-            <Th
-              color={useColorModeValue("#434347", "#A8ADBD")}
+            <Th>
+              <Box  display={"flex"}
+                  alignItems={"center"}>
+              <Text  color={useColorModeValue("#434347", "#A8ADBD")}
               fontSize={"10px"}
               fontWeight={"400"}
               letterSpacing={"1px"}
-            >
+              mr="6px">
               METHOD
+              </Text>
+             
+              <>
+              <img  width={8}
+                    height={8}
+                    alt='logo'
+                    src={useColorModeValue("/images/updown-triangleicon-light.png","/images/updown-triangleicon-dark.png")}>
+              </img>
+              </>
+              </Box>
             </Th>
-            <Th
-              color={useColorModeValue("#434347", "#A8ADBD")}
+
+
+            <Th>
+              <Box  display={"flex"}
+                  alignItems={"center"}>
+
+              <Text  color={useColorModeValue("#434347", "#A8ADBD")}
               fontSize={"10px"}
               fontWeight={"400"}
               letterSpacing={"1px"}
-            >
+              mr="6px"
+              >
               ACCOUNT / TOKEN
+               </Text>
+              <>
+              <img  width={8}
+                    height={8}
+                    alt='logo'
+                    src={useColorModeValue("/images/updown-triangleicon-light.png","/images/updown-triangleicon-dark.png")}>
+              </img>
+              </>
+              </Box>
             </Th>
+            
+
             <Th
               color={useColorModeValue("#434347", "#A8ADBD")}
               fontSize={"10px"}
@@ -67,7 +103,7 @@ const TransactionPanelComponent = () => {
             </Th>
           </Tr>
         </Thead>
-        <Tbody>
+        <Tbody  bgColor={useColorModeValue("#FFF","#202020")}>
 
           <>
             <Tr>
@@ -209,18 +245,16 @@ const TransactionPanelComponent = () => {
                   alignItems={"center"}
                 >
                   <Text
-                    color={"#245F00"}
                     fontSize={"10px"}
                     fontWeight={"600"}
                     letterSpacing={"1px"}
                     ml="6px"
+                    color={value1 < 0 ? "#EF1E1E" : "#245F00"}
                   >
-                     +300 USD 
+                     {value1 >= 0 ? `+${value1} USD` : `${value1} USD`} 
                   </Text>
                 </Box>
               </Td>
-
-
             </Tr>
          
 
@@ -370,8 +404,9 @@ const TransactionPanelComponent = () => {
                     fontWeight={"600"}
                     letterSpacing={"1px"}
                     ml="6px"
+                    color={value2 < 0 ? "#EF1E1E" : "#245F00"}
                   >
-                    +300 USD
+                    {value2 >= 0 ? `+${value2} USD` : `${value2} USD`} 
                   </Text>
                 </Box>
               </Td>
@@ -526,8 +561,9 @@ const TransactionPanelComponent = () => {
                     fontWeight={"600"}
                     letterSpacing={"1px"}
                     ml="6px"
+                    color={value3 < 0 ? "#EF1E1E" : "#245F00"}
                   >
-                    +300 USD
+                   {value3 >= 0 ? `+${value3} USD` : `${value3} USD`} 
                   </Text>
                 </Box>
               </Td>
