17886ab60cbc478bf29b6482f5bfd96369973f37
sidrawat73 <sid.rawat.sr2>
2023-08-07 17:33:19 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ require('dotenv').config()
 const webpack = require('webpack');
 const nextConfig = {
   output: "export",
+  output : {
+    publicPath:"/"
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
