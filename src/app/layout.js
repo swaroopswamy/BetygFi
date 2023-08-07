6ca35b32018de997c7e440cf50400e20c4b5ab85
sidrawat73 <sid.rawat.sr2>
2023-08-07 14:36:44 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,16 +20,20 @@ export default function RootLayout({ children }) {
   return (
     <>
       <html lang="en">
-        <body>
-          <ReduxProvider>
-            <Web3Provider>
-              <Providers>
-                <LayoutProvider>{children}</LayoutProvider>
-              </Providers>
-            </Web3Provider>
-          </ReduxProvider>
-        </body>
-      </html>
-    </>
-  );
+        <link rel="apple-touch-icon" sizes="180x180" href="../../../public/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="../../../public/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="../../../public/favicon/favicon-16x16.png" />
+              <link rel="manifest" href="/public/favicon/site.webmanifest" />
+                <body>
+                  <ReduxProvider>
+                    <Web3Provider>
+                      <Providers>
+                        <LayoutProvider>{children}</LayoutProvider>
+                      </Providers>
+                    </Web3Provider>
+                  </ReduxProvider>
+                </body>
+              </html>
+            </>
+            );
 }
--- src/services/walletDashboardService.js
+++ src/services/walletDashboardService.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { axiosInstance } from "../../util/axiosInstance";
-export const getWalletBalanceData = async (address,payload) => {
+export const getWalletBalanceData = async (address, payload) => {
   try {
     const { data } = await axiosInstance.post(
-      `wallet/balance/${address}`,payload
+      `wallet/balance/${address}`, payload
     );
     return data;
   } catch (err) {
-    return err;
+    return rejectWithValue(err);
   }
 };
 
-export const getWalletTransactionsData = async (address,payload) => {
+export const getWalletTransactionsData = async (address, payload) => {
   try {
     const { data } = await axiosInstance.post(
       `wallet/transactions/${address}/get`
     );
     return data;
   } catch (err) {
-    return err;
+    return rejectWithValue(err);
   }
 };
 
