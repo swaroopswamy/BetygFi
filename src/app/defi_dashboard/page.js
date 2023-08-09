2b234ea96c42a6bbb9568717fe04c4b0fb22a995
sidrawat73 <siddhant.rawat@solvendo.io>
2023-08-08 22:11:03 -0700

    changes

--- src/app/defi_dashboard/page.js
+++ src/app/defi_dashboard/page.js
@@ -1,7 +1,6 @@
 "use client"
-import { Box, Image, Tab, TabList, TabPanel, TabPanels, Tabs, Text, useColorModeValue, useColorMode } from "@chakra-ui/react";
 import Governance from "./governance";
-import React from "react";
+
 
 import React, { useCallback, useEffect, useState } from "react";
 import { Box, Image, Tab, TabList, TabPanel, TabPanels, Tabs, Text, useColorModeValue, useColorMode, Tooltip, Menu, MenuButton, MenuList, MenuItem, Checkbox } from "@chakra-ui/react";
