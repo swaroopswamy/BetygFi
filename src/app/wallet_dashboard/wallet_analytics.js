a16358efc31f531773f3ef8ce8b761baed25a122
unknown <swaroop.s@solvendo.io>
2023-08-11 23:02:07 +0530

    swaroop changes

--- src/app/wallet_dashboard/wallet_analytics.js
+++ src/app/wallet_dashboard/wallet_analytics.js
@@ -56,11 +56,10 @@ const WalletAnalyticsPanel = () => {
             justifyContent={"space-between"}
             alignItems={"center"}
             p="22px 25px"
-            paddingBottom={"0px"}
           >
             <Text
               fontSize={"15px"}
-              fontWeight={400}
+              fontWeight={"400"}
               lineHeight={"20px"}
               _dark={{ color: "#FFF" }}
               _light={{ color: "#212121" }}
@@ -78,7 +77,7 @@ const WalletAnalyticsPanel = () => {
               View More
             </Button> */}
           </Box>
-          <Box paddingTop={"15px"}>
+          <Box paddingLeft={"15px"}>
             <BlockchainAllocationTreemapChart />
           </Box>
         </Box>
