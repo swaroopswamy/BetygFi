bc9637f5c1f315cfd9c86e8abe185483c73725af
vathsa <shrivathsa.ms@solvendo.io>
2023-08-24 13:50:00 +0530

    Added horizontal line to defi dashboard

--- src/app/defi_dashboard/page.js
+++ src/app/defi_dashboard/page.js
@@ -384,7 +384,8 @@ const DefiDashboardPage = () => {
                         </Text>
                     </Box>
                 </Box>
-                <Image src={useColorModeValue("/images/Horizontalline(light).svg","/images/Horizontalline(Dark).svg")}></Image>
+                <hr style={colorMode === 'light' ? ({background:"#BFBFBF", margin:"10px 30px"}) : ({background:"#2F2F2F", margin:"10px 30px"})} />
+                {/* <Image src={useColorModeValue("/images/Horizontalline(light).svg","/images/Horizontalline(Dark).svg")}></Image> */}
             </Box>
             <Box
                 _light={{
