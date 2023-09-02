85ba1bde1b477e0aea30d1c0b738a9e32778df47
sidrawat73 <sid.rawat.sr2>
2023-09-02 14:13:58 +0530

    tables commented

--- src/app/defi_dashboard/DefiAssetCompositionSmallTable.js
+++ src/app/defi_dashboard/DefiAssetCompositionSmallTable.js
@@ -3,202 +3,234 @@ import React from "react";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
-import {  Text, Flex, Box, useColorModeValue, Image, Spacer, Button, useColorMode, colorMode, Tooltip, Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableContainer, Skeleton} from "@chakra-ui/react";
+import { Text, Flex, Box, useColorModeValue, Image, Spacer, Button, useColorMode, colorMode, Tooltip, Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableContainer, Skeleton } from "@chakra-ui/react";
 import GenericSmallTableComponent from "./GenericSmallTable";
 
-function Assetcomposition () {
+function Assetcomposition() {
 
-  const tableName = "DeFi Asset Composition";
-  const thread = ["Asset Name","Share","Value"];
-  const Definitions= "DeFi Asset Composition: Asset composition matrix shows the distribution of value in different assets. "
-  const Tablepath = "/defi_dashboard/asset_composition"
-  
+    const tableName = "DeFi Asset Composition";
+    const thread = ["Asset Name", "Share", "Value"];
+    const Definitions = "DeFi Asset Composition: Asset composition matrix shows the distribution of value in different assets. "
+    const Tablepath = "/defi_dashboard/asset_composition"
+    const tableData = [
+        ["/images/Ethdefiusers.svg", "Ethereum", "+ 356,456,560 USD "],
+        ["/images/Trondefiusers.svg", "Tron", "+ 256,456,560 USD"],
+        ["/images/Binancedefiusers.svg", "BSC", "+ 46,456,560 USD"],
+        ["/images/Arbitrumdefiusers.svg", "Arbitrum", "+ 66,456,560 USD"],
+        ["/images/Polygondefiusers.svg", "Polygon", "+ 56,456,560 USD"],
+    ];
 
-    const AssetcompositionTableData = useSelector(
+    /* const AssetcompositionTableData = useSelector(
         (state) => state?.defiDashboardData?.DefiHotContractsTableData
-    )
- 
-  return (
-    <Box
-       width={"50%"}  
-       bgColor={useColorModeValue("#F0F0F5","#191919")}
-       borderColor={useColorModeValue("#F0F0F5","#191919")}
-    >
-       <GenericSmallTableComponent
+    ) */
+
+    return (
+        <Box
+            width={"50%"}
+            bgColor={useColorModeValue("#F0F0F5", "#191919")}
+            borderColor={useColorModeValue("#F0F0F5", "#191919")}
+        >
+            <GenericSmallTableComponent
                 tableName={tableName}
                 thread={thread}
-                tableData={AssetcompositionTableData}
+                tableData={tableData}
                 RowComponent={RowComponent}
                 Definitions={Definitions}
                 Tablepath={Tablepath}
-        />
-    </Box>
-  )
+            />
+        </Box>
+    )
 };
 export default Assetcomposition;
 
 function RowComponent({ tableData }) {
-    console.log(tableData);
-  return (
-      <>
-          {tableData?.isError && (
-                <>
-                    <Tr
-                    height={"200px"}
+
+    return (
+        /*  <>
+             {tableData?.isError && (
+                 <>
+                     <Tr
+                         height={"200px"}
+                     >
+                         <Td
+                             colSpan={3}
+                         >
+                             <Text
+                                 _light={{
+                                     color: "#16171B"
+                                 }}
+                                 _dark={{
+                                     color: "#FFF"
+                                 }}
+                                 fontSize={"20px"}
+                                 fontWeight={"400"}
+                                 letterSpacing={"1px"}
+                                 textAlign={"center"}
+                                 p="20px"
+                             >
+                                 No Data available
+                             </Text>
+                         </Td>
+                     </Tr>
+                 </>
+             )}
+             {tableData?.isLoading && (
+                 <>
+                     <SkeletonRow />
+                     <SkeletonRow />
+                     <SkeletonRow />
+                 </>
+             )}
+             {tableData?.isSuccess && tableData?.data?.data?.map((item, i) => {
+                 return (
+                     <>
+                         <TableRow
+                             key={i}
+                             asset={{ name: item[1], src: item[0] }}
+                             share={item[2]}
+                             value={item[3]}
+                         />
+                     </>
+                 )
+             })}
+         </> */
+        <>
+            <Tr
+                height={"250px"}
+            >
+                <Td
+                    colSpan={3}
+                >
+                    <Text
+                        _light={{
+                            color: "#16171B"
+                        }}
+                        _dark={{
+                            color: "#FFF"
+                        }}
+                        fontSize={"20px"}
+                        fontWeight={"400"}
+                        letterSpacing={"1px"}
+                        textAlign={"center"}
+                        p="20px"
+
                     >
-                        <Td
-                         colSpan={3}
-                        >
-                            <Text
-                                _light={{
-                                    color: "#16171B"
-                                }}
-                                _dark={{
-                                    color: "#FFF"
-                                }}
-                                fontSize={"20px"}
-                                fontWeight={"400"}
-                                letterSpacing={"1px"}
-                                textAlign={"center"}
-                                p="20px"
-                            >
-                                No Data available
-                            </Text>
-                        </Td>
-                    </Tr>
-                </>
-            )}
-            {tableData?.isLoading && (
-                <>
-                    <SkeletonRow />
-                    <SkeletonRow />
-                    <SkeletonRow />
-                </>
-            )}
-          {tableData?.isSuccess && tableData?.data?.data?.map((item, i) => {
-                            return (
-                                <>
-                                    <TableRow
-                                        key={i}
-                                        asset={{name:item[1],src:item[0]}}
-                                        share={item[2]}
-                                        value={item[3]}
-                                    />
-                                </>
-                            )
-                        })}
-     </>
-  )
+                        No Data available
+                    </Text>
+                </Td>
+            </Tr>
+        </>
+    )
 }
 
 function TableRow({ key, asset, share, value }) {
-  const [clicked, setClick] = useState(false);
-  const { colorMode } = useColorMode();
-  const router = useRouter();
+    const [clicked, setClick] = useState(false);
+    const { colorMode } = useColorMode();
+    const router = useRouter();
 
-  return (
-      <>
-          <Tr
-              key={key}
-              cursor={"pointer"}
-              bgColor={clicked ?
-                  (colorMode === "light" ? '#F5F5F7' : '#191919') :
-                  (colorMode === "light" ? '#FFFFFF' : '#202020')
-              }
-              onClick={() => { setClick(!clicked) }}
-              borderBottom={'1px'}
-              borderColor={useColorModeValue('#DFDFDF', '#313131')}
-              borderRadius={'2px'}
-          >
-              <Td>
-                  <Flex>
-                      <Box
-                          alignItems={"center"}
-                          display={"flex"}
-                          gap={"10px"}
-                      >
-                          <Image
-                              height={"24px"}
-                              width={"24px"}
-                              src={asset.src}
-                              alt="logo"
-                          >
-                          </Image>
-                          <Text
-                              _dark={{
-                                  color: "#FFFFFF"
-                              }}
-                              _light={{
-                                  color: "#16171B"
-                              }}
-                              fontSize={"14px"}
-                              fontStyle={"normal"}
-                              fontWeight={"400"}
-                              lineHeight={"20px"}
-                          >
-                              {asset.name}
-                          </Text>
-                      </Box>
-                  </Flex>
-              </Td>
+    return (
+        <>
+            <Tr
+                key={key}
+                cursor={"pointer"}
+                bgColor={clicked ?
+                    (colorMode === "light" ? '#F5F5F7' : '#191919') :
+                    (colorMode === "light" ? '#FFFFFF' : '#202020')
+                }
+                onClick={() => { setClick(!clicked) }}
+                borderBottom={'1px'}
+                borderColor={useColorModeValue('#DFDFDF', '#313131')}
+                borderRadius={'2px'}
+            >
+                <Td>
+                    <Flex>
+                        <Box
+                            alignItems={"center"}
+                            display={"flex"}
+                            gap={"10px"}
+                        >
+                            <Image
+                                height={"24px"}
+                                width={"24px"}
+                                src={asset.src}
+                                alt="logo"
+                            >
+                            </Image>
+                            <Text
+                                _dark={{
+                                    color: "#FFFFFF"
+                                }}
+                                _light={{
+                                    color: "#16171B"
+                                }}
+                                fontSize={"14px"}
+                                fontStyle={"normal"}
+                                fontWeight={"400"}
+                                lineHeight={"20px"}
+                            >
+                                {asset.name}
+                            </Text>
+                        </Box>
+                    </Flex>
+                </Td>
 
-              <Td>
-                  <Flex>
-                      <Box>
-                          <Text
-                              _dark={{
-                                  color: "#FFFFFF"
-                              }}
-                              _light={{
-                                  color: "#16171B"
-                              }}
-                              fontSize={"14px"}
-                              fontStyle={"normal"}
-                              fontWeight={"400"}
-                              lineHeight={"20px"}
-                          >
-                              {share}
-                          </Text>
-                      </Box>
-                  </Flex>
-              </Td>
+                <Td>
+                    <Flex>
+                        <Box>
+                            <Text
+                                _dark={{
+                                    color: "#FFFFFF"
+                                }}
+                                _light={{
+                                    color: "#16171B"
+                                }}
+                                fontSize={"14px"}
+                                fontStyle={"normal"}
+                                fontWeight={"400"}
+                                lineHeight={"20px"}
+                            >
+                                {share}
+                            </Text>
+                        </Box>
+                    </Flex>
+                </Td>
 
-              <Td>
-                  <Flex>
-                      <Box>
-                          <Text
-                              _dark={{
-                                  color: "#FFFFFF"
-                              }}
-                              _light={{
-                                  color: "#16171B"
-                              }}
-                              fontSize={"14px"}
-                              fontStyle={"normal"}
-                              fontWeight={"400"}
-                              lineHeight={"20px"}
-                          >
-                              {value}
-                          </Text>
-                      </Box>
-                  </Flex>
-              </Td>
+                <Td>
+                    <Flex>
+                        <Box>
+                            <Text
+                                _dark={{
+                                    color: "#FFFFFF"
+                                }}
+                                _light={{
+                                    color: "#16171B"
+                                }}
+                                fontSize={"14px"}
+                                fontStyle={"normal"}
+                                fontWeight={"400"}
+                                lineHeight={"20px"}
+                            >
+                                {value}
+                            </Text>
+                        </Box>
+                    </Flex>
+                </Td>
 
-          </Tr>
-      </>
-  );
+            </Tr>
+        </>
+    );
 }
 
 const SkeletonRow = () => (
     <Box as="tr">
-      <Td>
-        <Skeleton height="20px" my={4} />
-      </Td>
-      <Td>
-        <Skeleton height="20px" my={4} />
-      </Td>
-      <Td>
-        <Skeleton height="20px" my={4} />
-      </Td>
+        <Td>
+            <Skeleton height="20px" my={4} />
+        </Td>
+        <Td>
+            <Skeleton height="20px" my={4} />
+        </Td>
+        <Td>
+            <Skeleton height="20px" my={4} />
+        </Td>
     </Box>
-  )
+)
