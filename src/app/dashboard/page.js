7e6608f9da42f0ebd53aa177d43468a1055d4d54
sidrawat73 <sid.rawat.sr2>
2023-09-06 11:13:15 +0530

    responsive changes

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -2,11 +2,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
+
   Box,
   Button,
   Checkbox,
@@ -476,7 +472,8 @@ const Dashboard = () => {
           </Box>
           <Box
             display={"inline-flex"}
-            mb={"30px"}>
+            mb={"30px"}
+          >
             <Box
               w="62%"
               display={{ base: "none", md: "block" }}
@@ -572,110 +569,8 @@ const Dashboard = () => {
                 </Text>
               </Box>
 
-              <Accordion>
-                <AccordionItem
-                  w="62%"
-                  display={{ base: "block", md: "none" }}
-                  borderRadius={"4px"}
-                  bgColor={useColorModeValue("#FFFFFF", "#202020")}
-                  filter={"filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.05));"}
-                  px={{ base: "10px", md: "20px" }}
-                  py={{ base: "10px", md: "25px" }}
-                  mr={{ base: "10px", md: "25px" }}
-                >
-                  <AccordionIcon />
-                  <AccordionButton>
-                    <Box
-                      display={"flex"}
-                      justifyContent={"space-between"}
-                      alignItems={"center"}
-                    >
-                      <Text
-                        color={useColorModeValue("#16171B", "#FFF")}
-                        fontSize={"18px"}
-                        fontWeight={"600"}
-                        lineHeight={"20px"}
-                      >
-                        Overview
-                      </Text>
-                      <Box display={"flex"} alignItems={"center"}>
-                        <Text
-                          color={useColorModeValue("#16171B", "#FFF")}
-                          fontSize={"14px"}
-                          fontWeight={"400"}
-                          lineHeight={"20px"}
-                          mr="7px"
-                        >
-                          Total Market Cap
-                        </Text>
-                        <Tooltip
-                          bgColor={useColorModeValue("rgba(97, 97, 97, 0.92)", "#FFF")}
-                          padding="4px 8px"
-                          label="Total Market Cap tracked by Solvendo"
-                          fontWeight={400}
-                          fontSize={"10px"}
-                          mr="7px"
-                        >
-                          <Image
-                            src={"/icons/info_sm_icon.svg"}
-                            width={12}
-                            height={12}
-                            alt='info-icon'
-                            style={{ marginRight: "7px" }}
-                          ></Image>
-                        </Tooltip>
-                        <Text
-                          color={useColorModeValue("#16171B", "#FFF")}
-                          fontSize={"24px"}
-                          fontWeight={"600"}
-                          lineHeight={"20px"}
-                          letterSpacing={"2.4px"}
-                        >
-                          {overviewData?.tvl ?
 
-                            /* (Math.trunc(overviewData?.tvl)).toLocaleString('en-US', {
-                              style: 'currency',
-                              currency: 'USD'
-                            }) */
-                            <>
-                              ${""}{millify(overviewData?.tvl, {
-                                precision: 2,
-                                locales: "en-US"
-                              })}
-                            </>
-
-                            :
-                            (
-                              <>
-                                NA
-                              </>
-                            )
-                          }
-                        </Text>
-                      </Box>
-                    </Box>
-                  </AccordionButton>
-                  <AccordionPanel>
-                    <Box
-                      bg={"#00000014"}
-                      p="30px"
-                      mt='30px'
-                    >
-                      <Text
-                        color={useColorModeValue("#16171B", "#FFF")}
-                        fontSize={"18px"}
-                        fontWeight={300}
-                        textAlign={"center"}
-                        opacity={0.6}
-                      >
-                        For the Risk Trend to be launched, the system need to run for a minimum duration of 4 weeks.
-                      </Text>
-                    </Box>
-                  </AccordionPanel>
-                </AccordionItem>
-              </Accordion>
             </Box>
-
             <Box
               w="35%"
               display={{ base: "none", md: "block" }}
@@ -696,9 +591,118 @@ const Dashboard = () => {
               <OverviewColumnChart />
             </Box>
 
-            <Accordion>
+
+          </Box>
+          <Box
+            display={"flex"}
+            mb={"20px"}
+            flexDirection={"column"}
+          >
+            <Accordion w={{ base: "100%", md: "62%" }}>
               <AccordionItem
-                w="35%"
+                w={{ base: "100%", md: "62%" }}
+                display={{ base: "block", md: "none" }}
+                borderRadius={"4px"}
+                bgColor={useColorModeValue("#FFFFFF", "#202020")}
+                filter={"filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.05));"}
+                px={{ base: "10px", md: "20px" }}
+                py={{ base: "10px", md: "25px" }}
+                mr={{ base: "10px", md: "25px" }}
+              >
+                <AccordionIcon />
+                <AccordionButton>
+                  <Box
+                    display={"flex"}
+                    justifyContent={"space-between"}
+                    alignItems={"center"}
+                  >
+                    <Text
+                      color={useColorModeValue("#16171B", "#FFF")}
+                      fontSize={"18px"}
+                      fontWeight={"600"}
+                      lineHeight={"20px"}
+                    >
+                      Overview
+                    </Text>
+                    <Box display={"flex"} alignItems={"center"}>
+                      <Text
+                        color={useColorModeValue("#16171B", "#FFF")}
+                        fontSize={"14px"}
+                        fontWeight={"400"}
+                        lineHeight={"20px"}
+                        mr="7px"
+                      >
+                        Total Market Cap
+                      </Text>
+                      <Tooltip
+                        bgColor={useColorModeValue("rgba(97, 97, 97, 0.92)", "#FFF")}
+                        padding="4px 8px"
+                        label="Total Market Cap tracked by Solvendo"
+                        fontWeight={400}
+                        fontSize={"10px"}
+                        mr="7px"
+                      >
+                        <Image
+                          src={"/icons/info_sm_icon.svg"}
+                          width={12}
+                          height={12}
+                          alt='info-icon'
+                          style={{ marginRight: "7px" }}
+                        ></Image>
+                      </Tooltip>
+                      <Text
+                        color={useColorModeValue("#16171B", "#FFF")}
+                        fontSize={"24px"}
+                        fontWeight={"600"}
+                        lineHeight={"20px"}
+                        letterSpacing={"2.4px"}
+                      >
+                        {overviewData?.tvl ?
+
+                          /* (Math.trunc(overviewData?.tvl)).toLocaleString('en-US', {
+                            style: 'currency',
+                            currency: 'USD'
+                          }) */
+                          <>
+                            ${""}{millify(overviewData?.tvl, {
+                              precision: 2,
+                              locales: "en-US"
+                            })}
+                          </>
+
+                          :
+                          (
+                            <>
+                              NA
+                            </>
+                          )
+                        }
+                      </Text>
+                    </Box>
+                  </Box>
+                </AccordionButton>
+                <AccordionPanel>
+                  <Box
+                    bg={"#00000014"}
+                    p="30px"
+                    mt='30px'
+                  >
+                    <Text
+                      color={useColorModeValue("#16171B", "#FFF")}
+                      fontSize={"18px"}
+                      fontWeight={300}
+                      textAlign={"center"}
+                      opacity={0.6}
+                    >
+                      For the Risk Trend to be launched, the system need to run for a minimum duration of 4 weeks.
+                    </Text>
+                  </Box>
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+            <Accordion w={{ base: "100%", md: "35%" }}>
+              <AccordionItem
+                w={{ base: "100%", md: "35%" }}
                 display={{ base: "block", md: "none" }}
                 borderRadius={"4px"}
                 bgColor={useColorModeValue("#FFFFFF", "#202020")}
