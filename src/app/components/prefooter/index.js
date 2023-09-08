1a6ae0777c45d52a4055b34660176316c065ff1f
vathsa <shrivathsa.ms@solvendo.io>
2023-09-08 18:07:06 +0530

    Prefooter added

--- src/app/components/prefooter/index.js
+++ src/app/components/prefooter/index.js
@@ -1,12 +1,28 @@
-import { Box , Text} from "@chakra-ui/react";
+import { Box , Text, useColorMode} from "@chakra-ui/react";
 import React from "react";
 
 const Prefooter = () => {
+    const { colorMode } = useColorMode();
 
     return (
         <>
-            <Box>
-                <Text>
+            <Box
+                display={"flex"}
+                justifyContent={"center"}
+                alignItems={"center"}
+                position="relative"
+                padding={"10px"}
+                mb={{base: "70px", md: "30px"}}
+                w={"100%"}
+                mx={"3px"}
+                bgColor={colorMode === 'light' ? "#FFF" : "#191919"}
+            >
+                <Text
+                    fontSize={"14px"}
+                    lineHeight={"20px"}
+                    letterSpacing={"1.4"}
+                    color={"#FFF"}
+                >
                     -- End of Page --
                 </Text>
             </Box>
