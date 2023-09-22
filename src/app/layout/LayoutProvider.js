8959ad04ed04ded4eddda3a8120e1089a08e114b
vathsa <shrivathsa.ms@solvendo.io>
2023-09-22 17:01:02 +0530

    reverting sidebar changes

--- src/app/layout/LayoutProvider.js
+++ src/app/layout/LayoutProvider.js
@@ -51,35 +51,32 @@ export default function LayoutProvider({ children }) {
     <>
       <Box
         width="100%"
-        minH="100vh" 
-        bg={useColorModeValue("#F0F0F5", "#191919")}
+        minH="100vh" bg={useColorModeValue("#F0F0F5", "#191919")}
         display={"flex"}
       >
         <SidebarContent
           onClose={() => onClose}
           w={isMobileSidebarCollapsed ? "null" : "80%"}
-          h={"100vh"}
+          h={"100%"}
         />
-        
         <Box
           display={{ base: "none", md: isMobileSidebarCollapsed ? "flex" : "none" }}
           flexDirection={"column"}
-          // ml={isSidebarCollapsed ? 20 : 225}
+          // ml={"225px"}
+          ml={screenSize?.width < 1450 ?
+            0 :
+            (isSidebarCollapsed ? 20 : 225)
+          }
           w="100%"
-          overflowX={"clip"}
+          overflowX={"hidden"}
         >
           <Navbar onOpenMenu={onOpen} />
-          <Box 
-            p="0" 
-            bgColor={useColorModeValue("#FFF", "#131313")} 
-            w="100%"
-          >
+          <Box p="0" bgColor={useColorModeValue("#FFF", "#131313")} w="100%">
             {children}
             {/* <Prefooter /> */}
             <Footer />
           </Box>
         </Box>
-
         <Box
           display={{base:"flex",md:"none"}}
           flexDirection={"column"}
