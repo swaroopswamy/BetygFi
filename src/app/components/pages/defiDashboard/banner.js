dcdde78ea7e106ec960e2667d32f3b280528c5a2
vathsa <shrivathsa.ms@solvendo.io>
2023-10-11 12:42:47 +0530

    Chains plural change

--- src/app/components/pages/defiDashboard/banner.js
+++ src/app/components/pages/defiDashboard/banner.js
@@ -55,7 +55,7 @@ const Banner = () => {
                     <Box h={"80%"} display={"flex"} justifyContent={"center"} alignItems={"end"}>
                         <Box layerStyle={"center"} gap={"15px"} px={"15px"} borderRight={useColorModeValue("1px solid #BFBFBF", "1px solid #2F2F2F")}>
                             <Text variant={'h3'} color={useColorModeValue("#3A3A3A", "#A8ADBD")}>
-                                {defiData?.chains?.length ?? 0} chains
+                                {defiData?.chains?.length ?? 0} {defiData?.chains?.length > 1 ? "chains" : "chain"}
                             </Text>
                             <Box layerStyle={"center"} >
                                 <BlockchainSelectionMenu
