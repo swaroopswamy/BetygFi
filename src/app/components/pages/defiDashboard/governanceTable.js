53565094ef6e9f4c30d1203310ec0a460309647f
vathsa <shrivathsa.ms@solvendo.io>
2023-10-13 18:11:35 +0530

    gov useEffect fix

--- src/app/components/pages/defiDashboard/governanceTable.js
+++ src/app/components/pages/defiDashboard/governanceTable.js
@@ -46,7 +46,7 @@ const GovernanceTable = ({}) => {
   useEffect(() => {
     getDefiGovernanceTableDataHandler();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [tablePage]);
 
   return (
     <>
