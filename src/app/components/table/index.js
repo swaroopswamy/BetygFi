953a0ef0f351a58dd132b1c6049bd0a108f82583
sidrawat73 <sid.rawat.sr2>
2023-10-25 10:09:13 +0530

    using isMd in dashboard

--- src/app/components/table/index.js
+++ src/app/components/table/index.js
@@ -17,6 +17,7 @@ import {
   Spinner,
   useColorMode,
   Icon,
+  useMediaQuery,
 } from "@chakra-ui/react";
 import React from "react";
 import dynamic from "next/dynamic";
@@ -39,49 +40,136 @@ const GenericTable = ({
   isQueryInPendingState = false,
   bigTable = false,
 }) => {
+  const [isMd] = useMediaQuery("(min-width: 768px)");
   return (
     <>
-      <Table
-        variant="simple"
-        key={1}
-        w={{ base: "100%", md: "100%" }}
-        display={{ base: "none", md: "table" }}
-        minW={bigTable ? "1200px" : "unset"}
-      >
-        <Thead
-          bgColor={useColorModeValue("#F5F5F7", "#191919")}
-          position="sticky"
-          top={0}
-          zIndex={"99"}
+      {isMd ? (
+        <Table
+          variant="simple"
+          key={1}
+          w={{ base: "100%", md: "100%" }}
+          display={{ base: "none", md: "table" }}
+          minW={bigTable ? "1200px" : "unset"}
         >
-          <Tr>
-            {tableHeader.map((item, i) => {
-              return (
-                <Th key={i} border={"0px"}>
-                  <Box display={"flex"} alignItems={"center"} gap={"3px"}>
-                    <Text
-                      variant={"tableHead"}
-                      textTransform={"capitalize"}
-                      textAlign={"left"}
-                    >
-                      {item.label}
+          <Thead
+            bgColor={useColorModeValue("#F5F5F7", "#191919")}
+            position="sticky"
+            top={0}
+            zIndex={"99"}
+          >
+            <Tr>
+              {tableHeader.map((item, i) => {
+                return (
+                  <Th key={i} border={"0px"}>
+                    <Box display={"flex"} alignItems={"center"} gap={"3px"}>
+                      <Text
+                        variant={"tableHead"}
+                        textTransform={"capitalize"}
+                        textAlign={"left"}
+                      >
+                        {item.label}
+                      </Text>
+                      {item.isSortingEnabled && (
+                        <Icon
+                          as={HiSortDescending}
+                          boxSize={"16px"}
+                          alt="Sort"
+                        />
+                      )}
+                      {item.isTooltip && (
+                        <TooltipComp label={item?.tooltipLabel} />
+                      )}
+                    </Box>
+                  </Th>
+                );
+              })}
+            </Tr>
+          </Thead>
+
+          <Tbody border={"0px"} bgColor={useColorModeValue("#FFF", "#202020")}>
+            {(tableData?.isError || tableData === null) && (
+              <>
+                <Tr>
+                  <Td
+                    p="20px"
+                    textAlign={"center"}
+                    height={"245px"}
+                    colSpan={SkeletonRowsColumnsDesktop?.numColumns}
+                  >
+                    <Text variant={"noDataText"}>No data available</Text>
+                  </Td>
+                </Tr>
+              </>
+            )}
+            {tableData?.isLoading && (
+              <SkeletonTable
+                numColumns={SkeletonRowsColumnsDesktop?.numColumns}
+                numRows={SkeletonRowsColumnsDesktop?.numRows}
+              />
+            )}
+            {isQueryInPendingState && (
+              <Tr>
+                <Td
+                  minH={"212px"}
+                  colSpan={SkeletonRowsColumnsDesktop?.numColumns}
+                  textAlign={"center"}
+                  p="20px"
+                >
+                  <Box
+                    display={"flex"}
+                    flexDirection={"column"}
+                    alignItems={"center"}
+                    justifyContent={"center"}
+                  >
+                    <Text variant={"noDataText"} mt="44px" mb="20px">
+                      We are retrieving data from the Blockchain.
                     </Text>
-                    {item.isSortingEnabled && (
-                      <Icon as={HiSortDescending} boxSize={"16px"} alt="Sort" />
-                    )}
-                    {item.isTooltip && (
-                      <TooltipComp label={item?.tooltipLabel} />
-                    )}
+                    <Spinner
+                      thickness="4px"
+                      speed="0.65s"
+                      emptyColor="gray.200"
+                      color="blue.500"
+                      size="xl"
+                    />
+                    <Text variant={"noDataText"} mt="20px" mb="8px">
+                      This process might take approximately 2-3 minutes.
+                    </Text>
+                    <Text variant={"noDataText"} mb="50px">
+                      You have the option to wait or return later.
+                    </Text>
                   </Box>
-                </Th>
-              );
-            })}
-          </Tr>
-        </Thead>
+                </Td>
+              </Tr>
+            )}
+            {tableData?.isSuccess &&
+              (tableData?.data?.data?.length > 0 ? (
+                tableData?.data?.data.map((item, rowIndex) => {
+                  return (
+                    <TableRow key={rowIndex} item={item} rowIndex={rowIndex} />
+                  );
+                })
+              ) : (
+                <Tr>
+                  <Td
+                    p="20px"
+                    textAlign={"center"}
+                    height={"245px"}
+                    colSpan={SkeletonRowsColumnsDesktop?.numColumns}
+                  >
+                    <Text variant={"noDataText"}>No data available</Text>
+                  </Td>
+                </Tr>
+              ))}
+          </Tbody>
+        </Table>
+      ) : (
+        <Table variant={"unstyled"} display={{ base: "table", md: "none" }}>
+          <Thead bgColor={useColorModeValue("#F5F5F7", "#191919")}>
+            <TableHeaderRowMobile />
+          </Thead>
 
-        <Tbody border={"0px"} bgColor={useColorModeValue("#FFF", "#202020")}>
-          {(tableData?.isError || tableData === null) && (
-            <>
+          <Tbody bgColor={useColorModeValue("#FFF", "#202020")}>
+            {(tableData?.isError || tableData === null) && (
               <Tr>
                 <Td
                   p="20px"
@@ -92,131 +180,51 @@ const GenericTable = ({
                   <Text variant={"noDataText"}>No data available</Text>
                 </Td>
               </Tr>
-            </>
-          )}
-          {tableData?.isLoading && (
-            <SkeletonTable
-              numColumns={SkeletonRowsColumnsDesktop?.numColumns}
-              numRows={SkeletonRowsColumnsDesktop?.numRows}
-            />
-          )}
-          {isQueryInPendingState && (
-            <Tr>
-              <Td
-                minH={"212px"}
-                colSpan={SkeletonRowsColumnsDesktop?.numColumns}
-                textAlign={"center"}
-                p="20px"
-              >
-                <Box
-                  display={"flex"}
-                  flexDirection={"column"}
-                  alignItems={"center"}
-                  justifyContent={"center"}
-                >
-                  <Text variant={"noDataText"} mt="44px" mb="20px">
-                    We are retrieving data from the Blockchain.
-                  </Text>
-                  <Spinner
-                    thickness="4px"
-                    speed="0.65s"
-                    emptyColor="gray.200"
-                    color="blue.500"
-                    size="xl"
-                  />
-                  <Text variant={"noDataText"} mt="20px" mb="8px">
-                    This process might take approximately 2-3 minutes.
-                  </Text>
-                  <Text variant={"noDataText"} mb="50px">
-                    You have the option to wait or return later.
-                  </Text>
-                </Box>
-              </Td>
-            </Tr>
-          )}
-          {tableData?.isSuccess &&
-            (tableData?.data?.data?.length > 0 ? (
-              tableData?.data?.data.map((item, rowIndex) => {
-                return (
-                  <TableRow key={rowIndex} item={item} rowIndex={rowIndex} />
-                );
-              })
-            ) : (
-              <Tr>
-                <Td
-                  p="20px"
-                  textAlign={"center"}
-                  height={"245px"}
-                  colSpan={SkeletonRowsColumnsDesktop?.numColumns}
-                >
-                  <Text variant={"noDataText"}>No data available</Text>
-                </Td>
-              </Tr>
-            ))}
-        </Tbody>
-      </Table>
+            )}
+            {tableData?.isLoading && (
+              <SkeletonTable
+                numColumns={SkeletonRowsColumnsMobile.numColumns}
+                numRows={SkeletonRowsColumnsMobile.numRows}
+              />
+            )}
 
-      <Table variant={"unstyled"} display={{ base: "table", md: "none" }}>
-        <Thead bgColor={useColorModeValue("#F5F5F7", "#191919")}>
-          <TableHeaderRowMobile />
-        </Thead>
-
-        <Tbody bgColor={useColorModeValue("#FFF", "#202020")}>
-          {(tableData?.isError || tableData === null) && (
-            <Tr>
-              <Td
-                p="20px"
-                textAlign={"center"}
-                height={"245px"}
-                colSpan={SkeletonRowsColumnsDesktop?.numColumns}
-              >
-                <Text variant={"noDataText"}>No data available</Text>
-              </Td>
-            </Tr>
-          )}
-          {tableData?.isLoading && (
-            <SkeletonTable
-              numColumns={SkeletonRowsColumnsMobile.numColumns}
-              numRows={SkeletonRowsColumnsMobile.numRows}
-            />
-          )}
-
-          {tableData?.isSuccess &&
-            (tableData?.data?.data?.length > 0 ? (
-              tableData?.data?.data.map((item, rowIndex) => {
-                return (
-                  <Tr key={rowIndex}>
-                    <Td p={0} colSpan={3}>
-                      <SingleAccordionComp
-                        display={"flex"}
-                        minH={"50px"}
-                        w={"100%"}
-                        borderRadius={"0px"}
-                        ButtonComp={() => {
-                          return <ButtonComp item={item} />;
-                        }}
-                        PanelComp={() => {
-                          return <PanelComp item={item} />;
-                        }}
-                      />
-                    </Td>
-                  </Tr>
-                );
-              })
-            ) : (
-              <Tr>
-                <Td
-                  p="20px"
-                  textAlign={"center"}
-                  height={"245px"}
-                  colSpan={SkeletonRowsColumnsDesktop?.numColumns}
-                >
-                  <Text variant={"noDataText"}>No data available</Text>
-                </Td>
-              </Tr>
-            ))}
-        </Tbody>
-      </Table>
+            {tableData?.isSuccess &&
+              (tableData?.data?.data?.length > 0 ? (
+                tableData?.data?.data.map((item, rowIndex) => {
+                  return (
+                    <Tr key={rowIndex}>
+                      <Td p={0} colSpan={3}>
+                        <SingleAccordionComp
+                          display={"flex"}
+                          minH={"50px"}
+                          w={"100%"}
+                          borderRadius={"0px"}
+                          ButtonComp={() => {
+                            return <ButtonComp item={item} />;
+                          }}
+                          PanelComp={() => {
+                            return <PanelComp item={item} />;
+                          }}
+                        />
+                      </Td>
+                    </Tr>
+                  );
+                })
+              ) : (
+                <Tr>
+                  <Td
+                    p="20px"
+                    textAlign={"center"}
+                    height={"245px"}
+                    colSpan={SkeletonRowsColumnsDesktop?.numColumns}
+                  >
+                    <Text variant={"noDataText"}>No data available</Text>
+                  </Td>
+                </Tr>
+              ))}
+          </Tbody>
+        </Table>
+      )}
     </>
   );
 };
