4fa2b9665c1f81cf0efa703d10b2add9ac9f8498
vathsa <shrivathsa.ms@solvendo.io>
2023-10-27 14:27:40 +0530

    Added reader

--- src/app/approach-paper/page.js
+++ src/app/approach-paper/page.js
@@ -35,37 +35,114 @@ const Approach = () => {
   const { colorMode } = useColorMode();
   const rendition = useRef(null);
   const [location, setLocation] = useState(0);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(null);
+  const [scale, setScale] = useState(1);
 
   useEffect(() => {
     if (rendition.current) {
       const themes = rendition.current.themes;
-      console.log("call again", themes);
+      console.log("h", themes);
       themes.override('color', colorMode === 'light' ? "#000000" : "#FFFFFF");
       themes.override('background', colorMode === 'light' ? "#FFFFFF" : "#191919");
+      
+      rendition.current.themes.register('custom', {
+        h1: {
+          'font-family': 'Inter, sans-serif',
+          'font-weight': '400',
+          'font-size': `${scale*24}px`,
+        },
+        p: {
+          'font-family': 'Inter, sans-serif',
+          'font-weight': '400',
+          'font-size': `${scale*14}px`,
+        },
+        body: {
+          'font-family': 'Inter, sans-serif',
+          'font-weight': '400',
+          'font-size': `${scale*14}px`,
+          'color': "#FF0000 !imporant" 
+        },
+        text_1: {
+          'font-family': 'Inter, sans-serif',
+          'font-weight': '400',
+          'font-size': `${scale*14}px`,
+          'color': "#FF0000"
+        }
+      })
+      rendition.current.themes.select('custom')
     }
   })
 
+  const changePage = (epubcfi) => {
+    setLocation(epubcfi);
+    if (rendition.current) {
+      const { displayed, href } = rendition.current.location.start;
+      setTotalPages(displayed.total);
+      setPage(displayed.page);
+    }
+  }
+
+  if (rendition.current) {
+    rendition.current.on('rendered', () => {
+      const { displayed, href } = rendition.current.location.start;
+      setTotalPages(displayed.total);
+      setPage(displayed.page);
+  })}
+
   return (
-    <Box display={"flex"} h={"85vh"} justifyContent={"center"} p={"25px 35px"} paddingRight={"200px"} bgColor={useColorModeValue("#F0F0F5", "#191919")}>
-      <Box display={"flex"} flexDir={"column"} w={"100%"} h={"95%"}>
+    <Box display={"flex"} h={"150vh"} justifyContent={"center"} p={"25px 35px"} paddingRight={{base: "none", md: "200px"}} bgColor={useColorModeValue("#F0F0F5", "#191919")}>
+      <Box display={"flex"} flexDir={"column"} w={"100%"} h={"95%"}  boxShadow={useColorModeValue("0px", "0px 0px 4px 0px #FFF")}>
         <ReactReader
-          url="https://react-reader.metabits.no/files/alice.epub"
+          url="/text/paper4.epub"
           showToc={false}
           location={location}
-          locationChanged={(epubcfi) => setLocation(epubcfi)}
+          locationChanged={changePage}
           readerStyles={colorMode === 'light' ? lightReaderTheme : darkReaderTheme}
           getRendition={(_rendition) => {
             rendition.current = _rendition;
           }}
+          epubOptions={{spread: "none"}}
         />
 
-        <Box layerStyle={"center"} minH={"5%"} bg={useColorModeValue("#C6C6C6", "#333333")} px={"20px"}>
-          <Slider aria-label='slider-ex-1' defaultValue={location} min={0} max={300} step={1}>
-            <SliderTrack>
-              <SliderFilledTrack />
-            </SliderTrack>
-            <SliderThumb />
-          </Slider>
+        <Box display={"flex"} alignItems={"center"} justifyContent={"center"} minH={"30px"} bg={useColorModeValue("#C6C6C6", "#333333")} px={"20px"}>
+          {/* <Box layerStyle={"center"} mx={"20px"}>
+            <Text variant="h3">
+              {page}/{totalPages}
+            </Text>
+          </Box>
+
+          <Box mx={"10px"} w={"70%"}>
+            <Slider aria-label='slider-ex-1' min={1} max={totalPages} step={1} onChange={(e) => {
+                if (e-page > 0)
+                  rendition.current.next();
+                else
+                  rendition.current.prev();
+              }}
+            >
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <SliderThumb />
+            </Slider>
+          </Box> */}
+
+
+          <Box mx={"10px"} w={"20%"} display={"flex"} gap={"10px"}>
+            <Text fontSize={"24px"}> - </Text>
+            <Slider aria-label='slider-ex-2' min={1} max={2} step={0.1}
+              onChange={(e) => {
+                setScale(e);
+              }}
+            >
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <SliderThumb />
+            </Slider>
+            <Text fontSize={"24px"}> + </Text>
+          </Box>
+
         </Box>
         </Box>
 
@@ -97,7 +174,7 @@ const darkReaderTheme = {
   readerArea: {
     ...ReactReaderStyle.readerArea,
     backgroundColor: '#191919',
-    transition: undefined,
+    transition: "undefined",
   },
   titleArea: {
     ...ReactReaderStyle.titleArea,
