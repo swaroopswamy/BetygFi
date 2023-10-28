733b45ea6032cb4e08ab45cbd067e4af0ea45d0b
anupamlata <anupam28195@gmail.com>
2023-10-28 17:52:38 +0530

    added cacheHandler

--- src/services/appService.js
+++ src/services/appService.js
@@ -1,16 +1,19 @@
 import { axiosInstance } from "@util/axiosInstance";
+import { cacheHandler, checkIfCacheAvailable } from "../../util/cacheHelper";
 
 export const getBlockchainListData = async () => {
     try {
-        const { data } = await axiosInstance.get(
-            `protocols/blockchains`
-        );
-        return data;
+        const url = `protocols/blockchains`;
+        if (checkIfCacheAvailable(url)) {
+            return checkIfCacheAvailable(url);
+        } else {
+            const { data } = await axiosInstance.get(url);
+            return cacheHandler(url, data, 4, false);
+        }
     } catch (err) {
         return rejectWithValue(err);
     }
 };
-
 
 export default {
     getBlockchainListData
