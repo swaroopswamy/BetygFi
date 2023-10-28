beb46f853eca778bf125e307988d593dca3058f1
sidrawat73 <sid.rawat.sr2>
2023-10-28 12:22:00 +0530

    color fixes and slider fixes

--- src/app/approach-paper/page.js
+++ src/app/approach-paper/page.js
@@ -25,14 +25,13 @@ import { TriangleUpIcon } from "@chakra-ui/icons";
 import { useEffect, useRef, useState } from "react";
 import dynamic from "next/dynamic";
 const ReactReader = dynamic(
-  () => import('react-reader').then((res) => res.ReactReader),
+  () => import("react-reader").then((res) => res.ReactReader),
   { ssr: false }
 );
 import { ReactReaderStyle } from "react-reader";
 
 let lightReaderTheme = {};
 let darkReaderTheme = {};
-
 
 const Approach = () => {
   const { colorMode } = useColorMode();
@@ -43,40 +42,46 @@ const Approach = () => {
   const [scale, setScale] = useState(1);
 
   useEffect(() => {
-
     if (rendition.current) {
       const themes = rendition.current.themes;
       console.log("h", themes);
-      themes.override('color', colorMode === 'light' ? "#000000" : "rgba(255, 255, 255, 0.8)");
-      themes.override('background', colorMode === 'light' ? "#FFFFFF" : "#191919");
-      
-      rendition.current.themes.register('custom', {
-        h1: {
-          'font-family': 'Inter, sans-serif',
-          'font-weight': '400',
-          'font-size': `${scale*24}px`,
+      themes.override(
+        "color",
+        colorMode === "light" ? "#000000" : "rgba(255, 255, 255, 0.8)"
+      );
+      themes.override(
+        "background",
+        colorMode === "light" ? "#FFFFFF" : "#191919"
+      );
+
+      rendition.current.themes.register("custom", {
+        ".block_": {
+          "font-family": "Inter, sans-serif",
+          "font-weight": "600",
+          "font-size": `${scale * 24}px`,
+          color: `${colorMode === "light" ? "#090909" : "#FFFFFF"}`,
         },
         p: {
-          'font-family': 'Inter, sans-serif',
-          'font-weight': '400',
-          'font-size': `${scale*14}px`,
+          "font-family": "Inter, sans-serif",
+          "font-weight": "400",
+          "font-size": `${scale * 14}px`,
         },
         body: {
-          'font-family': 'Inter, sans-serif',
-          'font-weight': '400',
-          'font-size': `${scale*14}px`,
-          'color': "#FF0000 !imporant" 
+          "font-family": "Inter, sans-serif",
+          "font-weight": "400",
+          "font-size": `${scale * 14}px`,
+          color: "#FF0000 !imporant",
         },
         text_1: {
-          'font-family': 'Inter, sans-serif',
-          'font-weight': '400',
-          'font-size': `${scale*14}px`,
-          'color': "#FF0000"
-        }
-      })
-      rendition.current.themes.select('custom')
+          "font-family": "Inter, sans-serif",
+          "font-weight": "400",
+          "font-size": `${scale * 14}px`,
+          color: "#FF0000",
+        },
+      });
+      rendition.current.themes.select("custom");
     }
-  })
+  });
 
   const changePage = (epubcfi) => {
     setLocation(epubcfi);
@@ -85,76 +90,97 @@ const Approach = () => {
       setTotalPages(displayed.total);
       setPage(displayed.page);
     }
-  }
+  };
 
   if (rendition.current) {
-    rendition.current.on('rendered', () => {
+    rendition.current.on("rendered", () => {
       const { displayed, href } = rendition.current.location.start;
       setTotalPages(displayed.total);
       setPage(displayed.page);
-  })}
-  
+    });
+  }
+
   return (
-    <Box display={"flex"} h={"150vh"} justifyContent={"center"} p={"25px 35px"} paddingRight={{base: "none", md: "200px"}} bgColor={useColorModeValue("#F0F0F5", "#191919")}>
-      <Box display={"flex"} flexDir={"column"} w={"100%"} h={"95%"}  boxShadow={useColorModeValue("0px", "0px 0px 4px 0px #FFF")}>
+    <Box
+      display={"flex"}
+      h={"150vh"}
+      justifyContent={"center"}
+      p={"25px 35px"}
+      paddingRight={{ base: "none", md: "200px" }}
+      bgColor={useColorModeValue("#F0F0F5", "#191919")}
+    >
+      <Box
+        display={"flex"}
+        flexDir={"column"}
+        w={"100%"}
+        h={"95%"}
+        boxShadow={useColorModeValue("0px", "0px 0px 4px 0px #FFF")}
+      >
         <ReactReader
           url="/text/paper4.epub"
           showToc={false}
           location={location}
           locationChanged={changePage}
-          readerStyles={colorMode === 'light' ? 
-            {
-              ...ReactReaderStyle,
-              readerArea: {
-                ...ReactReaderStyle.readerArea,
-                transition: undefined,
-              }
-            }
-            : 
-            {
-              ...ReactReaderStyle,
-              arrow: {
-                ...ReactReaderStyle.arrow,
-                color: 'white',
-              },
-              arrowHover: {
-                ...ReactReaderStyle.arrowHover,
-                color: '#ccc',
-              },
-              readerArea: {
-                ...ReactReaderStyle.readerArea,
-                backgroundColor: '#191919',
-                transition: "undefined",
-              },
-              titleArea: {
-                ...ReactReaderStyle.titleArea,
-                color: '#ccc',
-              },
-              tocArea: {
-                ...ReactReaderStyle.tocArea,
-                background: '#111',
-              },
-              tocButtonExpanded: {
-                ...ReactReaderStyle.tocButtonExpanded,
-                background: '#222',
-              },
-              tocButtonBar: {
-                ...ReactReaderStyle.tocButtonBar,
-                background: '#fff',
-              },
-              tocButton: {
-                ...ReactReaderStyle.tocButton,
-                color: 'white',
-              },
-            }
+          readerStyles={
+            colorMode === "light"
+              ? {
+                  ...ReactReaderStyle,
+                  readerArea: {
+                    ...ReactReaderStyle.readerArea,
+                    transition: undefined,
+                  },
+                }
+              : {
+                  ...ReactReaderStyle,
+                  arrow: {
+                    ...ReactReaderStyle.arrow,
+                    color: "white",
+                  },
+                  arrowHover: {
+                    ...ReactReaderStyle.arrowHover,
+                    color: "#ccc",
+                  },
+                  readerArea: {
+                    ...ReactReaderStyle.readerArea,
+                    backgroundColor: "#191919",
+                    transition: "undefined",
+                  },
+                  titleArea: {
+                    ...ReactReaderStyle.titleArea,
+                    color: "#ccc",
+                  },
+                  tocArea: {
+                    ...ReactReaderStyle.tocArea,
+                    background: "#111",
+                  },
+                  tocButtonExpanded: {
+                    ...ReactReaderStyle.tocButtonExpanded,
+                    background: "#222",
+                  },
+                  tocButtonBar: {
+                    ...ReactReaderStyle.tocButtonBar,
+                    background: "#fff",
+                  },
+                  tocButton: {
+                    ...ReactReaderStyle.tocButton,
+                    color: "white",
+                  },
+                }
           }
           getRendition={(_rendition) => {
             rendition.current = _rendition;
           }}
-          epubOptions={{spread: "none"}}
+          epubOptions={{ spread: "none" }}
         />
 
-        <Box display={"flex"} alignItems={"center"} justifyContent={"center"} minH={"30px"} bg={useColorModeValue("#C6C6C6", "#333333")} px={"20px"}>
+        <Box
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          minH={"30px"}
+          bg={useColorModeValue("#C6C6C6", "#333333")}
+          px={"20px"}
+        >
           {/* <Box layerStyle={"center"} mx={"20px"}>
             <Text variant="h3">
               {page}/{totalPages}
@@ -176,18 +202,26 @@ const Approach = () => {
             </Slider>
           </Box> */}
 
-
-          <Box mx={"10px"} w={"20%"} display={"flex"} gap={"10px"}>
-            <Text fontSize={"24px"} cursor={"pointer"} onClick={() => {
-              let s = scale;
-              if (s > 1) {
-                s = s - 0.1;
-                setScale(s);
-              }
-            }}>
-              - 
+          <Box mx={"10px"} w={{base:"70%",md:"20%"}} display={"flex"} gap={"10px"}>
+            <Text
+              fontSize={"24px"}
+              cursor={"pointer"}
+              onClick={() => {
+                let s = scale;
+                if (s > 1) {
+                  s = s - 0.1;
+                  setScale(s);
+                }
+              }}
+            >
+              -
             </Text>
-            <Slider aria-label='slider-ex-2' value={scale} min={1} max={1.5} step={0.05}
+            <Slider
+              aria-label="slider-ex-2"
+              value={scale}
+              min={1}
+              max={1.5}
+              step={0.05}
               onChange={(e) => {
                 setScale(e);
               }}
@@ -197,22 +231,23 @@ const Approach = () => {
               </SliderTrack>
               <SliderThumb />
             </Slider>
-            <Text fontSize={"24px"} cursor={"pointer"} onClick={() => {
-              let s = scale;
-              if (s < 1.5) {
-                s = s + 0.1;
-                setScale(s);
-              }
-            }}>
-            + 
+            <Text
+              fontSize={"24px"}
+              cursor={"pointer"}
+              onClick={() => {
+                let s = scale;
+                if (s < 1.5) {
+                  s = s + 0.1;
+                  setScale(s);
+                }
+              }}
+            >
+              +
             </Text>
           </Box>
-
         </Box>
-        </Box>
-
+      </Box>
     </Box>
-
   );
 };
 
