abf776518602ecdc7c618444af705d81e494c171
Shrivathsa M S <shrivathsa.ms@solvendo.io>
2023-10-31 11:08:14 +0000

    420/feature/tvl borrow api

--- src/services/defiDashboardService.js
+++ src/services/defiDashboardService.js
@@ -1,69 +1,80 @@
 import { axiosInstance } from "@util/axiosInstance";
 
 export const getDefiData = async (payload) => {
-  try {
-    const { data } = await axiosInstance.post(
-      `protocols/${payload.id}/get`, payload
-    );
-    return data;
-  } catch (err) {
-    // return rejectWithValue(err);
-  }
+	try {
+		const { data } = await axiosInstance.post(
+			`protocols/${payload.id}/get`, payload
+		);
+		return data;
+	} catch (err) {
+		// return rejectWithValue(err);
+	}
 };
 
 export const getDefiUsersTableData = async (payload) => {
-  try {
-    const { data } = await axiosInstance.get(
-      `protocols/${payload.defi}/users?blockchain=${payload.blockchain}`
-    );
-    return data;
-  } catch (err) {
-    // return rejectWithValue(err);
-  }
+	try {
+		const { data } = await axiosInstance.get(
+			`protocols/${payload.defi}/users?blockchain=${payload.blockchain}`
+		);
+		return data;
+	} catch (err) {
+		// return rejectWithValue(err);
+	}
 };
 
 export const getDefiHotContractsTableData = async (payload) => {
-  try {
-    const { data } = await axiosInstance.get(
-      `protocols/${payload.defi}/hotFunctions?blockchain=${payload.blockchain}`
-    );
-    return data;
-  } catch (err) {
-    // return rejectWithValue(err);
-  }
+	try {
+		const { data } = await axiosInstance.get(
+			`protocols/${payload.defi}/hotFunctions?blockchain=${payload.blockchain}`
+		);
+		return data;
+	} catch (err) {
+		// return rejectWithValue(err);
+	}
 };
 export const getDefiAssetCompositionTableData = async (payload) => {
-  try {
-    const { data } = await axiosInstance.get(
-      // eslint-disable-next-line max-len
-      `protocols/${payload.defi}/asset-composition?blockchain=${payload.blockchain}&page=${payload.page}&limit=${payload.limit}`
-    );
-    return data;
-  } catch (err) {
-    // return rejectWithValue(err);
-  }
+	try {
+		const { data } = await axiosInstance.get(
+			// eslint-disable-next-line max-len
+			`protocols/${payload.defi}/asset-composition?blockchain=${payload.blockchain}&page=${payload.page}&limit=${payload.limit}`
+		);
+		return data;
+	} catch (err) {
+		// return rejectWithValue(err);
+	}
 };
 
 export const getDefiFeeRevenueData = async (payload) => {
-  try {
-    const { data } = await axiosInstance.get(
-      `protocols/${payload.defi}/fees-revenue?blockchain=${payload.blockchain}`
-    );
-    return data;
-  } catch (err) {
-    // return rejectWithValue(err);
-  }
+	try {
+		const { data } = await axiosInstance.get(
+			`protocols/${payload.defi}/fees-revenue?blockchain=${payload.blockchain}`
+		);
+		return data;
+	} catch (err) {
+		// return rejectWithValue(err);
+	}
 };
 
 export const getDefiGovernanceTableData = async (payload) => {
-  try {
-    const { data } = await axiosInstance.get(
-      `protocols/${payload.defi}/governance?page=${payload.page}&limit=${payload.limit}`
-    );
-    return data;
-  } catch (err) {
-    // return rejectWithValue(err);
-  }
+	try {
+		const { data } = await axiosInstance.get(
+			`protocols/${payload.defi}/governance?page=${payload.page}&limit=${payload.limit}`
+		);
+		return data;
+	} catch (err) {
+		// return rejectWithValue(err);
+	}
+};
+
+export const getDefiTvlBorrowData = async (payload) => {
+	try {
+		const { data } = await axiosInstance.get(
+			`protocols/${payload.defi}/tvl-borrow?blockchain=${payload.blockchain}`
+		);
+		return data;
+	} catch (err) {
+		// return rejectWithValue(err);
+	}
 };
 
 
