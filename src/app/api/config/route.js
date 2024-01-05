b85cb8ec592e08392f8b81e862e07ab2b7153173
Suraj D. Patel <suraj.dp@solvendo.io>
2024-01-05 19:38:57 +0530

    ip updates

--- src/app/api/config/route.js
+++ src/app/api/config/route.js
@@ -1,5 +1,5 @@
 export async function GET() {
-    const res = await fetch(`http://54.178.13.3:4000/config/dashboard?env=${process.env.BUILD_ENV}`, {
+    const res = await fetch(`http://65.2.78.115:4000/config/dashboard?env=${process.env.BUILD_ENV}`, {
         headers: { 'Content-Type': 'application/json' },
         cache: 'no-store'
     });
