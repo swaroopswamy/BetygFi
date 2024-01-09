cf364e2b712b2ff749b38a5e90bf0cfac3602c52
Suraj D. Patel <suraj.dp@solvendo.io>
2024-01-09 23:30:56 +0530

    webpack removed

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-const webpack = require("webpack");
-
 const nextConfig = {
   async headers() {
     return [
