f4e834e19404c263a05498f8a793b12deff5be83
Suraj D. Patel <suraj.dp@solvendo.io>
2024-01-10 19:38:37 +0530

    app-configuration url updates

--- src/services/appService.js
+++ src/services/appService.js
@@ -63,7 +63,7 @@ export const getSearchV2TrendingData = async (payloadData, rejectWithValue) => {
 
 export const getAppConfig = async host => {
 	try {
-		const url = `${host}/api/config`;
+		const url = `${host}/api/app-configuration`;
 		if (checkIfCacheAvailable(url)) {
 			return checkIfCacheAvailable(url);
 		} else {
