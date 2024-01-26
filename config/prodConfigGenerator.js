4019b95be8357a7d0c86f7db85f78d0e94784316
Suraj D. Patel <suraj.dp@solvendo.io>
2024-01-26 11:51:01 +0530

    ip fix

--- config/prodConfigGenerator.js
+++ config/prodConfigGenerator.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 const VALID_KEY_LIST = ["NEXTAUTH_SECRET", "NEXTAUTH_URL"];
-const CONFIG_SERVER_IP_HOST = process.env.CONFIG_SERVER_IP_HOST;
-// const CONFIG_SERVER_IP_HOST = "http://52.66.250.16:4000";
+const CONFIG_SERVER_IP_HOST = process.env.CONFIG_SERVER_IP_HOST || "http://52.66.250.16:4000";
 const BUILD_ENV = "prod";
 const PORTAL_NAME = "dashboard";
 const URL = `${CONFIG_SERVER_IP_HOST}/config/${PORTAL_NAME}/${BUILD_ENV}`;
