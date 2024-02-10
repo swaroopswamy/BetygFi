9d7caf17709e00e2bea74c18bb8747b5746f7a61
Suraj D. Patel <suraj.dp@solvendo.io>
2024-02-10 10:50:01 +0530

    removed try catch

--- src/app/api/bet-dash/protocols/searchv2/route.js
+++ src/app/api/bet-dash/protocols/searchv2/route.js
@@ -1,15 +1,15 @@
 export async function GET(req) {
-    try {
-        const API_SERVICE_URL = process.env.API_SERVICE_URL;
-        const URL = API_SERVICE_URL + (req.url.split("bet-dash"))[1];
-        const fetchConfiguration = {
-            headers: { 'Content-Type': 'application/json' },
-            cache: 'no-store'
-        };
-        const res = await fetch(URL, fetchConfiguration);
-        const data = await res.json();
-        return Response.json(data);
-    } catch (error) {
-        return Response.json({ error });
-    }
+    // try {
+    const API_SERVICE_URL = process.env.API_SERVICE_URL;
+    const URL = API_SERVICE_URL + (req.url.split("bet-dash"))[1];
+    const fetchConfiguration = {
+        headers: { 'Content-Type': 'application/json' },
+        cache: 'no-store'
+    };
+    const res = await fetch(URL, fetchConfiguration);
+    const data = await res.json();
+    return Response.json(data);
+    // } catch (error) {
+    //   return Response.json({ error });
+    // }
 }
--- src/app/api/bet-dash/user/profile/route.js
+++ src/app/api/bet-dash/user/profile/route.js
@@ -1,22 +1,22 @@
 import { headers } from 'next/headers';
 
 export async function GET(req) {
-    try {
-        const headersList = headers();
-        const API_SERVICE_URL = process.env.API_SERVICE_URL;
-        const URL = API_SERVICE_URL + (req.url.split("bet-dash"))[1];
-        const authorization = headersList.get('authorization');
-        const fetchConfiguration = {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': authorization,
-            },
-            cache: 'no-store'
-        };
-        const res = await fetch(URL, fetchConfiguration);
-        const data = await res.json();
-        return Response.json(data);
-    } catch (error) {
-        return Response.json({ error });
-    }
+    // try {
+    const headersList = headers();
+    const API_SERVICE_URL = process.env.API_SERVICE_URL;
+    const URL = API_SERVICE_URL + (req.url.split("bet-dash"))[1];
+    const authorization = headersList.get('authorization');
+    const fetchConfiguration = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authorization,
+        },
+        cache: 'no-store'
+    };
+    const res = await fetch(URL, fetchConfiguration);
+    const data = await res.json();
+    return Response.json(data);
+    // } catch (error) {
+    //   return Response.json({ error });
+    // }
 }
