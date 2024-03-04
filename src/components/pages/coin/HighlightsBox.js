3c4fe9426537b53941746c3b1b31482751cb8a9b
unknown <swaroop.s@solvendo.io>
2024-03-04 16:03:22 +0530

    BET-1131 changed the file name of BTCDominance to BTC_ERF_SmallBox

--- src/components/pages/coin/HighlightsBox.js
+++ src/components/pages/coin/HighlightsBox.js
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import TopGainersSmallBox from "@/components/pages/coin/TopGainersSmallBox";
 import TopLosersSmallBox from "@/components/pages/coin/TopLosersSmallBox";
-import BTC_ETF_SmallBox from "@/components/pages/coin/BTCDominanceSmallBox";
+import BTC_ETF_SmallBox from "@/components/pages/coin/BTC_ETF_SmallBox";
 
 const HighlightsBox = () => {
     return (
