c6f71596a75edafaa78513118aa3ebfa52386cc2
unknown <siddhant.rawat@solvendo.io>
2024-04-15 21:57:38 +0530

    removed warning

--- src/components/pages/defiDashboard/ProtocolPage.js
+++ src/components/pages/defiDashboard/ProtocolPage.js
@@ -83,171 +83,169 @@ const ProtocolPage = () => {
     };
 
     return (
-        <>
-            <Box display={"flex"} flexDir={"column"} overflow={"hidden"}>
-                <Marquee />
-                <Box layerStyle={"flexCenter"} px={{ md: "14px" }} mb={{ base: "14px", md: "28px" }} >
-                    <BlockchainSelectionMenuNew w="95%" />
-                    <Box marginLeft={"-35px"}>
-                        <i className="icon arrow_right_grey" onClick={ScrollToRight} />
+        <Box display={"flex"} flexDir={"column"} overflow={"hidden"}>
+            <Marquee />
+            <Box layerStyle={"flexCenter"} px={{ md: "14px" }} mb={{ base: "14px", md: "28px" }} >
+                <BlockchainSelectionMenuNew w="95%" />
+                <Box marginLeft={"-35px"}>
+                    <i className="icon arrow_right_grey" onClick={ScrollToRight} />
+                </Box>
+                <Button onClick={onOpen} gap={"5px"} mr={"10px"} >
+                    <Image src={"/icons/filter_list.svg"} width={15} height={15} alt=" "></Image>
+                    <Text>Filter</Text>
+                </Button>
+            </Box>
+            <Box
+                display={"flex"}
+                flexDir={"column"}
+                bg={useColorModeValue("#F0F0F5", "#191919")}
+                px={{ base: "18px", md: "30px" }}
+                borderTop={"1px solid " + useColorModeValue("rgba(0, 0, 0, 0.1)", "rgba(255, 255, 255, 0.1)")}>
+                <Box layerStyle={"flexCenterSpaceBetween"} w="100%" mt={"20px"}>
+                    <Text variant={"contentHeading4"} fontSize={"20px"} lineHeight={"22px"}>
+                        DeFi Overview
+                    </Text>
+                    <Box layerStyle={"flexCenter"} w={{ base: "100%", md: "10%" }} justifyContent={"flex-end"}>
+                        <Text variant={"h3"} mr={"5px"} fontWeight={500}>Highlights</Text>
+                        <Switch
+                            size={"lg"}
+                            isChecked={isHighlightsBoxOpen}
+                            onChange={onHighlightsBoxToggle}
+                            className={colorMode === 'light' ? "custom-switch-light" : "custom-switch-dark"}
+                        ></Switch>
                     </Box>
-                    <Button onClick={onOpen} gap={"5px"} mr={"10px"} >
-                        <Image src={"/icons/filter_list.svg"} width={15} height={15} alt=" "></Image>
-                        <Text>Filter</Text>
-                    </Button>
+                </Box>
+                <Collapse in={isHighlightsBoxOpen}>
+                    <HighlightsBox />
+                </Collapse>
+                <Box
+                    display={"flex"}
+                    flexDirection={"column"}
+                    mt={{ base: "36px", md: "28px" }}
+                >
+                    <Text
+                        variant={"content"}
+                        _light={{ color: "#525252" }}
+                        _dark={{ color: "#FFFFFF" }}
+                        mb="12px"
+                    >
+                        Filter by DeFi Category
+                    </Text>
+                    <Box
+                        display={"flex"}
+                        overflow={"auto"}
+                        className="hidescrollbar"
+                    >
+                        <DashboardDefiSelection />
+                    </Box>
                 </Box>
                 <Box
                     display={"flex"}
-                    flexDir={"column"}
-                    bg={useColorModeValue("#F0F0F5", "#191919")}
-                    px={{ base: "18px", md: "30px" }}
-                    borderTop={"1px solid " + useColorModeValue("rgba(0, 0, 0, 0.1)", "rgba(255, 255, 255, 0.1)")}>
-                    <Box layerStyle={"flexCenterSpaceBetween"} w="100%" mt={"20px"}>
-                        <Text variant={"contentHeading4"} fontSize={"20px"} lineHeight={"22px"}>
-                            DeFi Overview
-                        </Text>
-                        <Box layerStyle={"flexCenter"} w={{ base: "100%", md: "10%" }} justifyContent={"flex-end"}>
-                            <Text variant={"h3"} mr={"5px"} fontWeight={500}>Highlights</Text>
-                            <Switch
-                                size={"lg"}
-                                isChecked={isHighlightsBoxOpen}
-                                onChange={onHighlightsBoxToggle}
-                                className={colorMode === 'light' ? "custom-switch-light" : "custom-switch-dark"}
-                            ></Switch>
-                        </Box>
-                    </Box>
-                    <Collapse in={isHighlightsBoxOpen}>
-                        <HighlightsBox />
-                    </Collapse>
-                    <Box
-                        display={"flex"}
-                        flexDirection={"column"}
-                        mt={{ base: "36px", md: "28px" }}
-                    >
-                        <Text
-                            variant={"content"}
-                            _light={{ color: "#525252" }}
-                            _dark={{ color: "#FFFFFF" }}
-                            mb="12px"
-                        >
-                            Filter by DeFi Category
-                        </Text>
-                        <Box
-                            display={"flex"}
-                            overflow={"auto"}
-                            className="hidescrollbar"
-                        >
-                            <DashboardDefiSelection />
-                        </Box>
-                    </Box>
-                    <Box
-                        display={"flex"}
-                        flexDir={{ base: "column", lg: "row" }}
-                        py={"15px"}
-                        gap={"15px"}
-                    >
-                        {/* <OverviewBox /> */}
-                        {/* <OverviewColumnChart /> */}
-                    </Box>
-                    <Rankings />
-                </Box >
-
-                <Modal isOpen={isOpen} onClose={onClose} size={"xl"}>
-                    <ModalOverlay />
-                    <ModalContent>
-                        <ModalHeader mt={"15px"} layerStyle={"flexCenter"}>
-                            <Text variant={"bigTextNumber"} lineHeight={"16px"}>Filter</Text>
-                            <Image width={18} height={18} src={"/icons/info_sm_icon.svg"} alt=" "></Image>
-                        </ModalHeader>
-                        <ModalCloseButton mt={"15px"} />
-                        <ModalBody>
-                            <Text variant={"contentHeading4"} fontSize={"20px"} lineHeight={"10px"}>
-                                DeFi Market
-                            </Text>
-                            <InputGroup width={"100%"}>
-                                <InputLeftElement my={"7px"}><Image src={"/icons/search.svg"} width={16} height={16} alt=" "></Image></InputLeftElement>
-                                <Input
-                                    bg={"#F0F0F5"}
-                                    mt={"15px"}
-                                    ml={"10px"}
-                                    width={"100%"}
-                                    placeholder='Search for asset'
-                                    size='xs'
-                                    color={colorMode === 'light' ? "#6F6F6F" : "#191919"}
-                                ></Input>
-                            </InputGroup>
-                            <Box mt={"15px"}>
-                                <Box layerStyle={"flexCenterSpaceBetween"} my={"20px"}>
-                                    <Box layerStyle={"flexCenter"}>
-                                        <Image src={"/icons/Menu_Icon.svg"} width={25} height={25} alt=" "></Image>
-                                        <Box layerStyle={"flexCenter"} ml={"10px"}>
-                                            <Image src={"/icons/bitcoin_logo.svg"} width={32} height={32} alt=" "></Image>
-                                            <Box m={"5px 0px 0px 5px"}>
-                                                <Text variant={"contentHeading4"} fontSize={"16px"} lineHeight={"10px"} >
-                                                    BTC
-                                                </Text>
-                                                <Text variant={"contentHeading4"} fontSize={"12px"} mt={"5px"} color={colorMode === 'light' ? "#757575" : "#A5A5A5"}>
-                                                    Bitcoin
-                                                </Text>
-                                            </Box>
-                                        </Box>
-                                    </Box>
-                                    <Image src={"/icons/Pin_Icon.svg"} width={24} height={24} alt=" "></Image>
-                                </Box>
-                                <Box layerStyle={"flexCenterSpaceBetween"} my={"20px"}>
-                                    <Box layerStyle={"flexCenter"}>
-                                        <Image src={"/icons/Menu_Icon.svg"} width={25} height={25} alt=" "></Image>
-                                        <Box layerStyle={"flexCenter"} ml={"10px"}>
-                                            <Image src={"/icons/bitcoin_logo.svg"} width={32} height={32} alt=" "></Image>
-                                            <Box m={"5px 0px 0px 5px"}>
-                                                <Text variant={"contentHeading4"} fontSize={"16px"} lineHeight={"10px"} >
-                                                    BTC
-                                                </Text>
-                                                <Text variant={"contentHeading4"} fontSize={"12px"} mt={"5px"} color={colorMode === 'light' ? "#757575" : "#A5A5A5"}>
-                                                    Bitcoin
-                                                </Text>
-                                            </Box>
-                                        </Box>
-                                    </Box>
-                                    <Image src={"/icons/Pin_Icon.svg"} width={24} height={24} alt=" "></Image>
-                                </Box>
-                                <Box layerStyle={"flexCenterSpaceBetween"} my={"20px"}>
-                                    <Box layerStyle={"flexCenter"}>
-                                        <Image src={"/icons/Menu_Icon.svg"} width={25} height={25} alt=" "></Image>
-                                        <Box layerStyle={"flexCenter"} ml={"10px"}>
-                                            <Image src={"/icons/bitcoin_logo.svg"} width={32} height={32} alt=" "></Image>
-                                            <Box m={"5px 0px 0px 5px"}>
-                                                <Text variant={"contentHeading4"} fontSize={"16px"} lineHeight={"10px"} >
-                                                    BTC
-                                                </Text>
-                                                <Text variant={"contentHeading4"} fontSize={"12px"} mt={"5px"} color={colorMode === 'light' ? "#757575" : "#A5A5A5"}>
-                                                    Bitcoin
-                                                </Text>
-                                            </Box>
-                                        </Box>
-                                    </Box>
-                                    <Image src={"/icons/Pin_Icon.svg"} width={24} height={24} alt=" "></Image>
-                                </Box>
-                            </Box>
-                        </ModalBody>
-
-                        <Box layerStyle={"flexCenterSpaceBetween"} m={"40px 20px 10px 20px"}>
-                            <Button variant={"modalButton"} width={100} height={30}>
-                                Reset
-                            </Button>
-                            <Box layerStyle={"flexCenter"} gap={"10px"}>
-                                <Button variant={"modalButton"} onClick={onClose} width={100} height={30}>
-                                    Cancel
-                                </Button>
-                                <Button variant={"modalButton"} width={100} height={30}>
-                                    Apply
-                                </Button>
-                            </Box>
-                        </Box>
-                    </ModalContent>
-                </Modal>
+                    flexDir={{ base: "column", lg: "row" }}
+                    py={"15px"}
+                    gap={"15px"}
+                >
+                    {/* <OverviewBox /> */}
+                    {/* <OverviewColumnChart /> */}
+                </Box>
+                <Rankings />
             </Box >
-        </>
+
+            <Modal isOpen={isOpen} onClose={onClose} size={"xl"}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader mt={"15px"} layerStyle={"flexCenter"}>
+                        <Text variant={"bigTextNumber"} lineHeight={"16px"}>Filter</Text>
+                        <Image width={18} height={18} src={"/icons/info_sm_icon.svg"} alt=" "></Image>
+                    </ModalHeader>
+                    <ModalCloseButton mt={"15px"} />
+                    <ModalBody>
+                        <Text variant={"contentHeading4"} fontSize={"20px"} lineHeight={"10px"}>
+                            DeFi Market
+                        </Text>
+                        <InputGroup width={"100%"}>
+                            <InputLeftElement my={"7px"}><Image src={"/icons/search.svg"} width={16} height={16} alt=" "></Image></InputLeftElement>
+                            <Input
+                                bg={"#F0F0F5"}
+                                mt={"15px"}
+                                ml={"10px"}
+                                width={"100%"}
+                                placeholder='Search for asset'
+                                size='xs'
+                                color={colorMode === 'light' ? "#6F6F6F" : "#191919"}
+                            ></Input>
+                        </InputGroup>
+                        <Box mt={"15px"}>
+                            <Box layerStyle={"flexCenterSpaceBetween"} my={"20px"}>
+                                <Box layerStyle={"flexCenter"}>
+                                    <Image src={"/icons/Menu_Icon.svg"} width={25} height={25} alt=" "></Image>
+                                    <Box layerStyle={"flexCenter"} ml={"10px"}>
+                                        <Image src={"/icons/bitcoin_logo.svg"} width={32} height={32} alt=" "></Image>
+                                        <Box m={"5px 0px 0px 5px"}>
+                                            <Text variant={"contentHeading4"} fontSize={"16px"} lineHeight={"10px"} >
+                                                BTC
+                                            </Text>
+                                            <Text variant={"contentHeading4"} fontSize={"12px"} mt={"5px"} color={colorMode === 'light' ? "#757575" : "#A5A5A5"}>
+                                                Bitcoin
+                                            </Text>
+                                        </Box>
+                                    </Box>
+                                </Box>
+                                <Image src={"/icons/Pin_Icon.svg"} width={24} height={24} alt=" "></Image>
+                            </Box>
+                            <Box layerStyle={"flexCenterSpaceBetween"} my={"20px"}>
+                                <Box layerStyle={"flexCenter"}>
+                                    <Image src={"/icons/Menu_Icon.svg"} width={25} height={25} alt=" "></Image>
+                                    <Box layerStyle={"flexCenter"} ml={"10px"}>
+                                        <Image src={"/icons/bitcoin_logo.svg"} width={32} height={32} alt=" "></Image>
+                                        <Box m={"5px 0px 0px 5px"}>
+                                            <Text variant={"contentHeading4"} fontSize={"16px"} lineHeight={"10px"} >
+                                                BTC
+                                            </Text>
+                                            <Text variant={"contentHeading4"} fontSize={"12px"} mt={"5px"} color={colorMode === 'light' ? "#757575" : "#A5A5A5"}>
+                                                Bitcoin
+                                            </Text>
+                                        </Box>
+                                    </Box>
+                                </Box>
+                                <Image src={"/icons/Pin_Icon.svg"} width={24} height={24} alt=" "></Image>
+                            </Box>
+                            <Box layerStyle={"flexCenterSpaceBetween"} my={"20px"}>
+                                <Box layerStyle={"flexCenter"}>
+                                    <Image src={"/icons/Menu_Icon.svg"} width={25} height={25} alt=" "></Image>
+                                    <Box layerStyle={"flexCenter"} ml={"10px"}>
+                                        <Image src={"/icons/bitcoin_logo.svg"} width={32} height={32} alt=" "></Image>
+                                        <Box m={"5px 0px 0px 5px"}>
+                                            <Text variant={"contentHeading4"} fontSize={"16px"} lineHeight={"10px"} >
+                                                BTC
+                                            </Text>
+                                            <Text variant={"contentHeading4"} fontSize={"12px"} mt={"5px"} color={colorMode === 'light' ? "#757575" : "#A5A5A5"}>
+                                                Bitcoin
+                                            </Text>
+                                        </Box>
+                                    </Box>
+                                </Box>
+                                <Image src={"/icons/Pin_Icon.svg"} width={24} height={24} alt=" "></Image>
+                            </Box>
+                        </Box>
+                    </ModalBody>
+
+                    <Box layerStyle={"flexCenterSpaceBetween"} m={"40px 20px 10px 20px"}>
+                        <Button variant={"modalButton"} width={100} height={30}>
+                            Reset
+                        </Button>
+                        <Box layerStyle={"flexCenter"} gap={"10px"}>
+                            <Button variant={"modalButton"} onClick={onClose} width={100} height={30}>
+                                Cancel
+                            </Button>
+                            <Button variant={"modalButton"} width={100} height={30}>
+                                Apply
+                            </Button>
+                        </Box>
+                    </Box>
+                </ModalContent>
+            </Modal>
+        </Box >
     );
 };
 
