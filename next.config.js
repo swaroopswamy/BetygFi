5fe044610e8eba86dfe591df32dbd6c6b789928b
unknown <siddhant.rawat@solvendo.io>
2024-05-07 18:37:57 +0530

    updated dev and qa notification redirect url

--- next.config.js
+++ next.config.js
@@ -10,11 +10,11 @@ const nextConfig = {
     return [
       {
         source: '/dev/notification/:path*',
-        destination: 'http://34.233.111.224:31924/:path*'
+        destination: 'http://10.40.59.142:30380/:path*'
       },
       {
         source: '/local/notification/:path*',
-        destination: 'http://34.233.111.224:31924/:path*'
+        destination: 'http://10.40.59.142:30380/:path*'
       },
       {
         source: '/qa/notification/:path*',
