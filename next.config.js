5b7fcb4636b922dea5b64c27c832ab99c39491c9
unknown <siddhant.rawat@solvendo.io>
2024-07-02 09:28:21 +0530

    added rewrite

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 const nextConfig = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$|.*\\.jpg$).*)"],
   async rewrites() {
     return [
       {
