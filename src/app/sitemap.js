43fea2a235a5e623478b7fdc0d1164d7adfd4588
Suraj D. Patel <suraj.dp@solvendo.io>
2024-07-02 12:26:40 +0530

    static sitemap added

---
+++ src/app/sitemap.js
@@ -0,0 +1,52 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://platform.betygfi.com',
+            lastModified: new Date(),
+            changeFrequency: 'hourly',
+            priority: 1,
+        },
+        {
+            url: 'https://platform.betygfi.com/top-wallets',
+            lastModified: new Date(),
+            changeFrequency: 'hourly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://platform.betygfi.com/protocol',
+            lastModified: new Date(),
+            changeFrequency: 'hourly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://platform.betygfi.com/coin',
+            lastModified: new Date(),
+            changeFrequency: 'hourly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://platform.betygfi.com/about',
+            lastModified: new Date(),
+            changeFrequency: 'hourly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://platform.betygfi.com/approach-paper',
+            lastModified: new Date(),
+            changeFrequency: 'hourly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://platform.betygfi.com/help',
+            lastModified: new Date(),
+            changeFrequency: 'hourly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://platform.betygfi.com/legal',
+            lastModified: new Date(),
+            changeFrequency: 'hourly',
+            priority: 0.5,
+        },
+    ];
+}
