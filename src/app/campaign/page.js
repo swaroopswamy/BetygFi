086f019a3e70f278be4f0c11b7e4fe80bdc34f0d
unknown <siddhant.rawat@solvendo.io>
2024-07-10 09:48:49 +0530

    campaign-landing-page

--- src/app/campaign/page.js
+++ src/app/campaign/page.js
@@ -3,13 +3,13 @@ import { Box } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
-const CampaignHeader = dynamic(() => import('@components/pages/campaign/header'), { ssr: false });
-const CampaignPageSection1 = dynamic(() => import('@components/pages/campaign/section1'), { ssr: false });
-const CampaignPageSection2 = dynamic(() => import('@components/pages/campaign/section2'), { ssr: false });
-const CampaignPageSection3 = dynamic(() => import('@components/pages/campaign/section3'), { ssr: false });
-const CampaignPageSection4 = dynamic(() => import('@components/pages/campaign/section4'), { ssr: false });
-const CampaignPageSection5 = dynamic(() => import('@components/pages/campaign/section5'), { ssr: false });
-const CampaignPageSection6 = dynamic(() => import('@components/pages/campaign/section6'), { ssr: false });
+const CampaignHeader = dynamic(() => import('@components/pages/campaign/header'));
+const CampaignPageSection1 = dynamic(() => import('@components/pages/campaign/section1'));
+const CampaignPageSection2 = dynamic(() => import('@components/pages/campaign/section2'));
+const CampaignPageSection3 = dynamic(() => import('@components/pages/campaign/section3'));
+const CampaignPageSection4 = dynamic(() => import('@components/pages/campaign/section4'));
+const CampaignPageSection5 = dynamic(() => import('@components/pages/campaign/section5'));
+const CampaignPageSection6 = dynamic(() => import('@components/pages/campaign/section6'));
 
 const CampaignPage = () => {
     const searchParams = useSearchParams();
