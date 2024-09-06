33f39579ace117310426af374bd13f04a37cb460
S Swaroop <swaroop.s@solvendo.io>
2024-09-06 20:44:47 +0530

    implementing saveTab Modal

--- src/components/pages/coin/CustomizeTabModal.js
+++ src/components/pages/coin/CustomizeTabModal.js
@@ -7,11 +7,12 @@ import Image from "next/image";
 
 const CustomizeTabLayoutPanel = dynamic(() => import("@components/pages/coin/CustomizeTabLayoutSettings"), { ssr: false });
 const CustomizeTabAssetsPanel = dynamic(() => import("@components/pages/coin/CustomizeTabAssets"), { ssr: false });
+const SaveTabModal = dynamic(() => import("@components/pages/coin/SaveTabModal"), { ssr: false });
 
-const CustomizeTabModal = ({ isCustomizeTabModalOpen, onCustomizeTabModalClose, onSaveTabModalOpen }) => {
-
+const CustomizeTabModal = ({ isCustomizeTabModalOpen, onCustomizeTabModalClose }) => {
     const { colorMode } = useColorMode();
     const [tabIndex, setTabIndex] = useState(0);
+    const [isSaveTabModalOpen, setIsSaveTabModalOpen] = useState(false);
     const ValidatedUserData = useSelector((state) => state.authData.ValidatedUserData);
     {
         ValidatedUserData?.AnnotationState &&
@@ -20,120 +21,134 @@ const CustomizeTabModal = ({ isCustomizeTabModalOpen, onCustomizeTabModalClose, 
             </Box>;
     }
 
+    const handleContinueClick = () => {
+        setIsSaveTabModalOpen(true); // Open the SaveTabModal
+    };
+
+    const handleSaveTabModalClose = () => {
+        setIsSaveTabModalOpen(false); // Close the SaveTabModal
+    };
+
     return (
-        <Modal isOpen={isCustomizeTabModalOpen} onClose={onCustomizeTabModalClose} >
-            <ModalOverlay
-                bg="blackAlpha.300"
-                backdropFilter="blur(10px) "
-            />
-            <ModalContent
-                zIndex={10000}
-                maxW={"800px"}
-                width={{ base: "90%" }}
-                padding={{ base: "10px", md: "20px 10px" }}
-                bgColor={colorMode === 'light' ? "#FFFFFF" : "#313131"}
-                position={{ md: "fixed" }}
-            >
-                <ModalHeader>
-                    <Text variant={"bigText"} fontSize={{ base: "24px" }} fontWeight={500} lineHeight={"16px"} letterSpacing={"0.32px"}>Tab Name</Text>
-                </ModalHeader>
-                <ModalCloseButton borderRadius={"50%"} backgroundColor={colorMode === 'light' ? "#F0F0F5" : "#191919"} mt={"10px"} />
-                <ModalBody px={{ base: "8px" }}>
-                    <Tabs onChange={(index) => setTabIndex(index)} mt={"15px"}>
-                        <TabList>
-                            <Tab padding={"0px"}>
-                                <Box
-                                    layerStyle={"flexCenter"}
-                                    borderLeftRadius={"4px"}
-                                    w={{ base: "100%", sm: "100%", midSize: "auto", md: "auto" }}
-                                    padding={{ base: "12px 6px", md: "13px 15px" }}
-                                    bgColor={tabIndex === 0 ?
-                                        (colorMode === 'light' ? "#202020" : "#FFFFFF") :
-                                        (colorMode === 'light' ? "#F0F0F5" : "#202020")
-                                    }
-                                >
-                                    <Text
-                                        variant={"TabText"}
-                                        color={tabIndex === 0 ?
-                                            (colorMode === 'light' ? "#FFFFFF" : "#000000") :
-                                            (colorMode === 'light' ? "#000000" : "#FFFFFF")
-                                        }
-                                    >
-                                        Layout
-                                    </Text>
+        <>
+            <Modal isOpen={isCustomizeTabModalOpen} onClose={onCustomizeTabModalClose} >
+                <ModalOverlay
+                    bg="blackAlpha.300"
+                    backdropFilter="blur(10px) "
+                />
+                <ModalContent
+                    zIndex={10000}
+                    maxW={"800px"}
+                    width={{ base: "90%" }}
+                    padding={{ base: "10px", md: "20px 10px" }}
+                    bgColor={colorMode === 'light' ? "#FFFFFF" : "#313131"}
+                    position={{ md: "fixed" }}
+                >
+                    <ModalHeader>
+                        <Text variant={"bigText"} fontSize={{ base: "24px" }} fontWeight={500} lineHeight={"16px"} letterSpacing={"0.32px"}>Create Tab</Text>
+                    </ModalHeader>
+                    <ModalCloseButton borderRadius={"50%"} backgroundColor={colorMode === 'light' ? "#F0F0F5" : "#191919"} mt={"10px"} />
+                    <ModalBody px={{ base: "8px" }}>
+                        <Tabs onChange={(index) => setTabIndex(index)} mt={"15px"}>
+                            <TabList>
+                                <Tab padding={"0px"}>
                                     <Box
-                                        display={{ base: "none", md: "block" }}
-                                        width={15}
-                                        height={15}
-                                        paddingY={"3px"}
-                                        borderRadius={"4px"}
+                                        layerStyle={"flexCenter"}
+                                        borderLeftRadius={"4px"}
+                                        w={{ base: "100%", sm: "100%", midSize: "auto", md: "auto" }}
+                                        padding={{ base: "12px 6px", md: "13px 15px" }}
                                         bgColor={tabIndex === 0 ?
-                                            (colorMode === 'light' ? "#FFFFFF" : "#191919") :
-                                            (colorMode === 'light' ? "#191919" : "#FFFFFF")
+                                            (colorMode === 'light' ? "#202020" : "#FFFFFF") :
+                                            (colorMode === 'light' ? "#F0F0F5" : "#202020")
                                         }
-                                        textAlign={"center"}
                                     >
                                         <Text
-                                            variant={"footnoteText"}
-                                            fontWeight={500}
-                                            lineHeight={"10px"}
+                                            variant={"TabText"}
                                             color={tabIndex === 0 ?
-                                                (colorMode === 'light' ? "#191919" : "#FFFFFF") :
-                                                (colorMode === 'light' ? "#FFFFFF" : "#191919")
-                                            }>
-                                            1
+                                                (colorMode === 'light' ? "#FFFFFF" : "#000000") :
+                                                (colorMode === 'light' ? "#000000" : "#FFFFFF")
+                                            }
+                                        >
+                                            Layout
                                         </Text>
+                                        <Box
+                                            display={{ base: "none", md: "block" }}
+                                            width={15}
+                                            height={15}
+                                            paddingY={"3px"}
+                                            borderRadius={"4px"}
+                                            bgColor={tabIndex === 0 ?
+                                                (colorMode === 'light' ? "#FFFFFF" : "#191919") :
+                                                (colorMode === 'light' ? "#191919" : "#FFFFFF")
+                                            }
+                                            textAlign={"center"}
+                                        >
+                                            <Text
+                                                variant={"footnoteText"}
+                                                fontWeight={500}
+                                                lineHeight={"10px"}
+                                                color={tabIndex === 0 ?
+                                                    (colorMode === 'light' ? "#191919" : "#FFFFFF") :
+                                                    (colorMode === 'light' ? "#FFFFFF" : "#191919")
+                                                }>
+                                                1
+                                            </Text>
+                                        </Box>
                                     </Box>
-                                </Box>
-                            </Tab>
-                            <Tab padding={"0px"}>
-                                <Box
-                                    layerStyle={"flexCenter"}
-                                    borderRightRadius={"4px"}
-                                    w={{ base: "100%", sm: "100%", midSize: "auto", md: "auto" }}
-                                    padding={{ base: "12px 6px 12px 6px", md: "13px 19px 12px 15x" }}
-                                    bgColor={tabIndex === 1 ?
-                                        (colorMode === 'light' ? "#202020" : "#FFFFFF") :
-                                        (colorMode === 'light' ? "#F0F0F5" : "#202020")
-                                    }
-                                >
-                                    <Text
-                                        variant={"TabText"}
-                                        color={tabIndex === 1 ?
-                                            (colorMode === 'light' ? "#FFFFFF" : "#000000") :
-                                            (colorMode === 'light' ? "#000000" : "#FFFFFF")
+                                </Tab>
+                                <Tab padding={"0px"}>
+                                    <Box
+                                        layerStyle={"flexCenter"}
+                                        borderRightRadius={"4px"}
+                                        w={{ base: "100%", sm: "100%", midSize: "auto", md: "auto" }}
+                                        padding={{ base: "12px 6px 12px 6px", md: "13px 19px 12px 15x" }}
+                                        bgColor={tabIndex === 1 ?
+                                            (colorMode === 'light' ? "#202020" : "#FFFFFF") :
+                                            (colorMode === 'light' ? "#F0F0F5" : "#202020")
                                         }
                                     >
-                                        Assets
-                                    </Text>
-                                </Box>
-                            </Tab>
-                        </TabList>
-                        <TabPanels>
-                            <TabPanel p="0px">
-                                <CustomizeTabLayoutPanel />
-                            </TabPanel>
-                            <TabPanel p="0px">
-                                <CustomizeTabAssetsPanel />
-                            </TabPanel>
-                        </TabPanels>
-                    </Tabs>
-                    <Box layerStyle={"flexCenterSpaceBetween"} m={"40px 20px 10px 20px"}>
-                        {/* <Button variant={"modalButton"} width={100} height={30}>
+                                        <Text
+                                            variant={"TabText"}
+                                            color={tabIndex === 1 ?
+                                                (colorMode === 'light' ? "#FFFFFF" : "#000000") :
+                                                (colorMode === 'light' ? "#000000" : "#FFFFFF")
+                                            }
+                                        >
+                                            Assets
+                                        </Text>
+                                    </Box>
+                                </Tab>
+                            </TabList>
+                            <TabPanels>
+                                <TabPanel p="0px">
+                                    <CustomizeTabLayoutPanel />
+                                </TabPanel>
+                                <TabPanel p="0px">
+                                    <CustomizeTabAssetsPanel />
+                                </TabPanel>
+                            </TabPanels>
+                        </Tabs>
+                        <Box layerStyle={"flexCenterSpaceBetween"} m={"40px 20px 10px 20px"}>
+                            {/* <Button variant={"modalButton"} width={100} height={30}>
                             Reset
                         </Button> */}
-                        <Box layerStyle={"flexCenter"} gap={"10px"} position="absolute" bottom="35px" right="25px">
-                            <Button display={{ base: "none", md: "block" }} variant={"modalButton"} width={100} height={30} onClick={onCustomizeTabModalClose}>
-                                Cancel
-                            </Button>
-                            <Button variant={"modalButton"} width={100} height={30} onClick={onSaveTabModalOpen}>
-                                Continue
-                            </Button>
+                            <Box layerStyle={"flexCenter"} gap={"10px"} position="absolute" bottom="35px" right="25px">
+                                <Button display={{ base: "none", md: "block" }} variant={"modalButton"} width={100} height={30} onClick={onCustomizeTabModalClose}>
+                                    Cancel
+                                </Button>
+                                <Button variant={"modalButton"} width={100} height={30} onClick={handleContinueClick}>
+                                    Continue
+                                </Button>
+                            </Box>
                         </Box>
-                    </Box>
-                </ModalBody>
-            </ModalContent>
-        </Modal >
+                    </ModalBody>
+                </ModalContent>
+            </Modal >
+            <SaveTabModal
+                isSaveTabModalOpen={isSaveTabModalOpen}
+                onSaveTabModalClose={handleSaveTabModalClose}
+            />
+        </>
     );
 };
 
--- src/components/pages/coin/SaveTabModal.js
+++ src/components/pages/coin/SaveTabModal.js
@@ -1,8 +1,9 @@
 "use client";
 import React from "react";
-import { Box, Text, useColorMode, Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay, ModalCloseButton } from "@chakra-ui/react";
+import { Box, Text, useColorMode, Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay, ModalCloseButton, Button } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import Image from "next/image";
+import InputText from "@components/settingsPage/editDetailsInput/input";
 
 const SaveTabModal = ({ isSaveTabModalOpen, onSaveTabModalClose }) => {
 
@@ -30,11 +31,49 @@ const SaveTabModal = ({ isSaveTabModalOpen, onSaveTabModalClose }) => {
                 position={{ md: "fixed" }}
             >
                 <ModalHeader>
-                    <Text variant={"bigText"} fontSize={{ base: "24px" }} fontWeight={500} lineHeight={"16px"} letterSpacing={"0.32px"}>Tab Name</Text>
+                    <Text variant={"bigText"} fontSize={{ base: "24px" }} fontWeight={500} lineHeight={"16px"} letterSpacing={"0.32px"}>Create Tab</Text>
                 </ModalHeader>
                 <ModalCloseButton borderRadius={"50%"} backgroundColor={colorMode === 'light' ? "#F0F0F5" : "#191919"} mt={"10px"} />
-                <ModalBody>
-                    <Text>Hello World</Text>
+                <ModalBody mt={"25px"}>
+                    <Text variant={"contentHeading4"} fontSize={{ base: "14px", md: "16px" }} lineHeight={"10px"}>
+                        Tab Name
+                    </Text>
+                    <Box w="100%" mt={"15px"}>
+                        <InputText
+                            placeholder={"Enter Tab Name"}
+                            type={"text"}
+                            name="tab_name"
+                        />
+                    </Box>
+                    <Box mt={"25px"}>
+                        <Text variant={"contentHeading4"} fontSize={{ base: "14px", md: "16px" }} lineHeight={"10px"}>
+                            Tab Description
+                        </Text>
+                        <Box w="100%" mt={"15px"}>
+                            <InputText
+                                placeholder={"Enter Tab Description (Optional)"}
+                                type={"textarea"}
+                                name="tab_description"
+                            />
+                        </Box>
+                    </Box>
+                    <Box layerStyle={"flexCenter"} mt={"40px"} gap={"10px"}>
+                        <Image width={34} height={36} src={colorMode === 'light' ? "/icons/share_light.svg" : "/icons/share_dark.svg"}></Image>
+                        <Box>
+                            <Text variant={"contentHeading4"} fontSize={"16px"} lineHeight={"10px"} pb={"2px"}>Public Access</Text>
+                            <Text variant={"SettingsText3"} fontWeight={500} lineHeight={"10px"} pt={"3px"}>
+                                Make your Tab visible for Community in Tab Library and via Sharing Link.
+                            </Text>
+                        </Box>
+                    </Box>
+                    <Box layerStyle={"flexCenter"} justifyContent={"flex-end"} mt={"40px"} gap={"10px"}>
+                        <Button display={{ base: "none", md: "block" }} variant={"modalButton"} width={100} height={30} onClick={onSaveTabModalClose}>
+                            Cancel
+                        </Button>
+                        <Button variant={"modalButton"} width={100} height={30}>
+                            Save Tab
+                        </Button>
+                    </Box>
                 </ModalBody>
             </ModalContent>
         </Modal >
